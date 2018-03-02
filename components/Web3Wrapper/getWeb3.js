b264667fac6c0ef70deb3f78bdfb4f32c4f8c9c9
Raymond Yeh <raymond_yeh@tech.gov.sg>
2018-03-02 09:51:28 +0800

    Added Web3Wrapper

---
+++ components/Web3Wrapper/getWeb3.js
@@ -0,0 +1,28 @@
+import Web3 from "web3";
+
+const resolveWeb3 = resolve => {
+  let { web3 } = window;
+  const alreadyInjected = typeof web3 !== "undefined"; // i.e. Mist/Metamask
+  const localProvider = `http://localhost:9545`;
+
+  if (alreadyInjected) {
+    web3 = new Web3(web3.currentProvider);
+  } else {
+    const provider = new Web3.providers.HttpProvider(localProvider);
+    web3 = new Web3(provider);
+  }
+
+  resolve(web3);
+};
+
+export default () =>
+  new Promise(resolve => {
+    // Wait for loading completion to avoid race conditions with web3 injection timing.
+    window.addEventListener(`load`, () => {
+      resolveWeb3(resolve);
+    });
+    // If document has loaded already, try to get Web3 immediately.
+    if (document.readyState === `complete`) {
+      resolveWeb3(resolve);
+    }
+  });
--- pages/newPage.js
+++ pages/newPage.js
@@ -2,18 +2,11 @@ import withRedux from "next-redux-wrapper";
 import initStore from "../store";
 import PageTitle from "../components/PageTitle";
 import PeopleTableContainer from "../components/PeopleTableContainer";
-import Web3Wrapper from "../components/Web3Wrapper";
-import contractDefinition from "../services/contracts/SimpleStorage.json";
-
-const PeopleTableContainerWithWeb3 = Web3Wrapper(
-  PeopleTableContainer,
-  contractDefinition
-);
 
 const IndexPage = () => (
   <div>
     <PageTitle text="Hello World" />
-    <PeopleTableContainerWithWeb3 />
+    <PeopleTableContainer />
   </div>
 );
 
