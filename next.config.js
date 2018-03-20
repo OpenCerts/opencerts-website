eb2a415240dbca76f21de1e77cf2822d09c62d29
Ng Guoyou <ng.guoyou@gmail.com>
2018-03-20 15:56:13 +0800

    Add Github pages deployment script

---
+++ next.config.js
@@ -0,0 +1,11 @@
+const isProd = process.env.NODE_ENV === "production";
+
+module.exports = {
+  exportPathMap: function exportMap() {
+    return {
+      "/": { page: "/" },
+      "/admin": { page: "/admin" }
+    };
+  },
+  assetPrefix: isProd ? "." : ""
+};
