cb85de0bf4eaefb38510b3c491445566d2adc8f5
Raymond Yeh <raymond_yeh@tech.gov.sg>
2018-08-13 15:07:34 +0800

    Styled footer and header

--- components/FooterBar.js
+++ components/FooterBar.js
@@ -1,7 +1,7 @@
 import NetworkSelector from "../components/NetworkSelector";
 
 const NavigationBar = () => (
-  <div className="bg-info d-flex p-1 m-0">
+  <div className="bg-dark d-flex p-1 m-0">
     <div className="ml-auto p-1">
       <NetworkSelector />
     </div>
--- components/NavigationBar.js
+++ components/NavigationBar.js
@@ -1,5 +1,41 @@
+// Ref: https://getbootstrap.com/docs/4.1/examples/navbars/
 const NavigationBar = () => (
-  <div className="bg-info h1 p-1 m-0">Navbar here</div>
+  <nav className="navbar navbar-expand-md navbar-dark bg-dark">
+    <a className="navbar-brand" href="#">
+      OpenCerts
+    </a>
+    <button
+      className="navbar-toggler"
+      type="button"
+      data-toggle="collapse"
+      data-target="#navbarsExample04"
+      aria-controls="navbarsExample04"
+      aria-expanded="false"
+      aria-label="Toggle navigation"
+    >
+      <span className="navbar-toggler-icon" />
+    </button>
+
+    <div className="collapse navbar-collapse" id="navbarsExample04">
+      <ul className="navbar-nav ml-auto">
+        <li className="nav-item active">
+          <a className="nav-link" href="#">
+            About <span className="sr-only">(current)</span>
+          </a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">
+            Registry
+          </a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">
+            Github
+          </a>
+        </li>
+      </ul>
+    </div>
+  </nav>
 );
 
 export default NavigationBar;
