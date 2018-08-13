e7e727712d82d8be0bf6da160987b99723ec1cdd
Raymond Yeh <raymond_yeh@tech.gov.sg>
2018-08-13 14:39:00 +0800

    Styled partners

--- components/ContentPage/PartnersSection.js
+++ components/ContentPage/PartnersSection.js
@@ -1,10 +1,18 @@
 const TitleSection = () => <div className="h2">Partners</div>;
 
+const CarouselSection = () => {
+  const images = Array(8).fill("http://via.placeholder.com/150x80");
+  const items = images.map((item, i) => (
+    <img src={item} key={i} className="m-2" />
+  ));
+  return <div className="d-flex flex-wrap justify-content-center">{items}</div>;
+};
+
 const PartnerSection = () => (
-  <div className="row">
+  <div className="row my-3 p-4">
     <div className="container-fluid">
       <TitleSection />
-      ------- Some scrolling shit here ------
+      <CarouselSection />
     </div>
   </div>
 );
