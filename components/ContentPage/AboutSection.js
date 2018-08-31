19a4187bee205e05bec6d08b04d9008022c8d250
Raymond Yeh <raymond_yeh@tech.gov.sg>
2018-08-31 10:57:01 +0800

    Refactored AboutSection

--- components/ContentPage/AboutSection.js
+++ components/ContentPage/AboutSection.js
@@ -6,59 +6,67 @@ const tabs = [
     content: <p>This is how you view</p>,
     id: "view-tab",
     control: "view"
+  },
+  {
+    title: "Verification Process",
+    content: <p>This is how you verify</p>,
+    id: "verify-tab",
+    control: "verify"
   }
 ];
+
+const renderTabTitle = () => {
+  const list = tabs.map((tab, i) => (
+    <li className="nav-item" key={i}>
+      <a
+        className={`nav-link ${i === 0 ? "active" : ""}`}
+        id={tab.id}
+        data-toggle="tab"
+        href={`#${tab.control}`}
+        role="tab"
+        aria-controls={tab.control}
+        aria-selected={i === 0 ? "true" : "false"}
+      >
+        {tab.title}
+      </a>
+    </li>
+  ));
+  return (
+    <ul className="nav nav-tabs" id="myTab" role="tablist">
+      {list}
+    </ul>
+  );
+};
+
+const renderTabContent = () => {
+  const list = tabs.map((tab, i) => (
+    <div
+      key={i}
+      className={`tab-pane fade ${i === 0 ? "show active" : ""}`}
+      id={tab.control}
+      role="tabpanel"
+      aria-labelledby={tab.id}
+    >
+      {tab.content}
+    </div>
+  ));
+  return (
+    <div
+      className="tab-content py-3"
+      id="myTabContent"
+      style={{ minHeight: 300 }}
+    >
+      {list}
+    </div>
+  );
+};
 
 const AboutSection = () => (
   <div className="row p-4 bg-light" id="how-it-works">
     <div className="container-fluid">
       <SectionTitle />
-      <ul className="nav nav-tabs" id="myTab" role="tablist">
-        <li className="nav-item">
-          <a
-            className="nav-link active"
-            id="view-tab"
-            data-toggle="tab"
-            href="#view"
-            role="tab"
-            aria-controls="view"
-            aria-selected="true"
-          >
-            Viewing A Certificate
-          </a>
-        </li>
-        <li className="nav-item">
-          <a
-            className="nav-link"
-            id="verification-tab"
-            data-toggle="tab"
-            href="#verification"
-            role="tab"
-            aria-controls="verification"
-            aria-selected="false"
-          >
-            Verification Process
-          </a>
-        </li>
-      </ul>
-      <div className="tab-content" id="myTabContent">
-        <div
-          className="tab-pane fade show active"
-          id="view"
-          role="tabpanel"
-          aria-labelledby="view-tab"
-        >
-          ...
-        </div>
-        <div
-          className="tab-pane fade"
-          id="verification"
-          role="tabpanel"
-          aria-labelledby="verification-tab"
-        >
-          ...
-        </div>
-      </div>
+      {renderTabTitle()}
+      {renderTabContent()}
     </div>
   </div>
 );
