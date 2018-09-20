2c253004b19c88678936c480718b26394f6b3671
angelw22 <angelwong9510@gmail.com>
2018-09-20 15:35:09 +0800

    Update some width styles

--- src/components/HomePageContent/DropZoneSection.js
+++ src/components/HomePageContent/DropZoneSection.js
@@ -6,24 +6,23 @@ const DropZoneSection = ({ handleCertificateChange }) => (
     className="row p-4 bg-brand-dark text-white"
     // style={{ boxShadow: "inset 0 0 50px 0 rgba(102, 120, 138, 0.2)" }}
   >
-    <div className="col">
-      <div className="mb-3 py-4">
-        <p>
-          This OpenCerts site allows you to view and verify the authenticity of
-          certificates.
-        </p>
-        <p>
-          If you have received a certificate, dropping it into the box on the
-          right will allow you to check its contents and verify its
-          authenticity.
-        </p>
+    <div className="main">
+      <div className="col">
+        <div className="mb-3 py-4">
+          <p>
+            This OpenCerts site allows you to view and verify the authenticity of
+            certificates.
+          </p>
+          <p>
+            If you have received a certificate, dropping it into the box on the
+            right will allow you to check its contents and verify its
+            authenticity.
+          </p>
+        </div>
       </div>
-      <div>
-        <a href="#how-it-works">Learn More</a>
+      <div className="col">
+        <CertificateDropzone handleCertificateChange={handleCertificateChange} />
       </div>
-    </div>
-    <div className="col">
-      <CertificateDropzone handleCertificateChange={handleCertificateChange} />
     </div>
   </div>
 );
