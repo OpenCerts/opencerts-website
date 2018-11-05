b6bcbe88a201c3a272ad2e094773abc93d4662cd
Raymond Yeh <raymond_yeh@tech.gov.sg>
2018-11-05 15:50:24 +0800

    Switched to main net and made network selector visible

--- src/components/Layout/FooterBar.js
+++ src/components/Layout/FooterBar.js
@@ -2,7 +2,10 @@ import NetworkSelector from "./NetworkSelector";
 import css from "./footer.scss";
 
 const NavigationBar = () => (
-  <div className="bg-brand-dark p-1 m-0" id={css["footer-print"]}>
+  <div className="bg-brand-dark py-3 m-0" id={css["footer-print"]}>
+    <div className={css.networkselector}>
+      <NetworkSelector />
+    </div>
     <div className={css.footer}>
       <div>
         <a href="/faq">FAQ</a>
@@ -12,9 +15,6 @@ const NavigationBar = () => (
       </div>
       <div>
         <a href="/registry">Registry</a>
-      </div>
-      <div className={css.networkselector}>
-        <NetworkSelector />
       </div>
     </div>
   </div>
