26f016d6954b3057e2d1fde5c3a1f6d8221aa572
yehjxraymond <yehjxraymond@gmail.com>
2018-11-07 15:24:11 +0800

    Added privacy page (#28)

--- src/components/Layout/FooterBar.js
+++ src/components/Layout/FooterBar.js
@@ -3,18 +3,21 @@ import css from "./footer.scss";
 
 const NavigationBar = () => (
   <div className="bg-brand-dark py-3 m-0" id={css["footer-print"]}>
-    <div className={css.networkselector}>
-      <NetworkSelector />
-    </div>
     <div className={css.footer}>
+      <div className={css.networkselector}>
+        <NetworkSelector />
+      </div>
       <div>
         <a href="/faq">FAQ</a>
       </div>
       <div>
-        <a href="https://github.com/OpenCerts/open-certificate">Github</a>
+        <a href="/registry">Registry</a>
       </div>
       <div>
-        <a href="/registry">Registry</a>
+        <a href="/privacy">Privacy Policy</a>
+      </div>
+      <div>
+        <a href="https://github.com/OpenCerts/open-certificate">Github</a>
       </div>
     </div>
   </div>
