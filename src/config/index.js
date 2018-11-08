87191edfb84aea58b2f510a14c96ae87f2f0281c
yehjxraymond <yehjxraymond@gmail.com>
2018-11-08 10:41:54 +0800

    Deployment toggle by ENV (#30)
    * Aded deployment variable
    
    * Added new cert store for NP
    
    * Changed from DEPLOY to ENV

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,9 @@
 import { types as Web3Types } from "../services/web3/getWeb3";
 
-export const DEFAULT_NETWORK = Web3Types.INFURA_MAINNET;
+export const DEFAULT_NETWORK =
+  process.env.ENV === "production"
+    ? Web3Types.INFURA_MAINNET
+    : Web3Types.INFURA_ROPSTEN;
 export const CAPTCHA_CLIENT_KEY = "6LfiL3EUAAAAAHrfLvl2KhRAcXpanNXDqu6M0CCS";
 export const EMAIL_API_URL =
   "https://ikeem3vgb5.execute-api.ap-southeast-1.amazonaws.com/dev";
