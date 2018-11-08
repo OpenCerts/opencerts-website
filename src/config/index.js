2608356c422f36fb1a991f18faf9e531f8e95255
Raymond Yeh <raymond_yeh@tech.gov.sg>
2018-11-08 10:29:47 +0800

    Changed from DEPLOY to ENV

--- src/config/index.js
+++ src/config/index.js
@@ -1,7 +1,7 @@
 import { types as Web3Types } from "../services/web3/getWeb3";
 
 export const DEFAULT_NETWORK =
-  process.env.DEPLOY === "production"
+  process.env.ENV === "production"
     ? Web3Types.INFURA_MAINNET
     : Web3Types.INFURA_ROPSTEN;
 export const CAPTCHA_CLIENT_KEY = "6LfiL3EUAAAAAHrfLvl2KhRAcXpanNXDqu6M0CCS";
