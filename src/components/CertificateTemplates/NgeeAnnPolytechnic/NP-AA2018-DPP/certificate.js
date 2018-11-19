1ac87fa0d2466bb65cea41a81c7977993252c7ef
Raymond Yeh <raymond_yeh@tech.gov.sg>
2018-11-19 11:20:48 +0800

    Lint & refactored cert

--- src/components/CertificateTemplates/NgeeAnnPolytechnic/NP-AA2018-DPP/certificate.js
+++ src/components/CertificateTemplates/NgeeAnnPolytechnic/NP-AA2018-DPP/certificate.js
@@ -1,38 +1,82 @@
-import { IMG_LOGO_NP } from "../common/images";
+import { get } from "lodash";
 import {
-  formatDate,
-  formatDatePrefix,
-  formatCertName,
-  formatCertID
-} from "../common/functions";
+  fullWidthStyle,
+  printTextStyle,
+  nameTextStyle,
+  titleTextStyle,
+  renderSingapore,
+  renderLogoNP,
+  renderIssuingDate,
+  renderFooter
+} from "../common/certificate";
 
-const fullWidthStyle = {
-  width: "100%",
-  height: "auto"
-};
+export const renderAwardText = certificate => (
+  <div>
+    <div
+      className="row d-flex justify-content-center"
+      style={{ marginTop: "3rem" }}
+    >
+      <p style={printTextStyle}>This is to certify that</p>
+    </div>
+    <div className="row d-flex justify-content-center">
+      <p style={nameTextStyle}>{certificate.recipient.name}</p>
+    </div>
+    <div className="row d-flex justify-content-center">
+      <p style={printTextStyle}>was awarded the</p>
+    </div>
+    <div className="row d-flex justify-content-center">
+      <p style={titleTextStyle}>
+        {" "}
+        Diploma Plus Certificate
+        <br />
+        in
+        <br />
+        {certificate.name}
+      </p>
+    </div>
+  </div>
+);
 
-const printTextStyle = {
-  fontWeight: "500!important",
-  fontWize: "1.2rem",
-  color: "#555",
-  textAlign: "center"
-};
-
-const singaporeTextStyle = {
-  color: "#555",
-  fontSize: "3rem"
-};
-
-const nameTextStyle = {
-  fontSize: "3rem",
-  textAlign: "center"
-};
-
-const titleTextStyle = {
-  color: "rgb(30,93,200)",
-  fontSize: "3rem",
-  textAlign: "center"
-};
+const renderSignatures = certificate => (
+  <div
+    className="row d-flex justify-content-center align-items-end"
+    style={{ marginTop: "8rem", marginBottom: "3rem" }}
+  >
+    <div className="col-1" />
+    <div className="col-4">
+      <div className="px-4">
+        <img
+          style={fullWidthStyle}
+          src={get(certificate, "additionalData.certSignatories[0].signature")}
+        />
+        <hr />
+      </div>
+      <div className="text-center">
+        {get(certificate, "additionalData.certSignatories[0].name")}
+      </div>
+      <div className="text-center">
+        {get(certificate, "additionalData.certSignatories[0].position")}
+      </div>
+    </div>
+    <div className="col-2" />
+    <div className="col-4">
+      <div className="px-4">
+        <img
+          style={fullWidthStyle}
+          src={get(certificate, "additionalData.certSignatories[1].signature")}
+        />
+        <hr />
+      </div>
+      <div className="text-center">
+        {get(certificate, "additionalData.certSignatories[1].name")}
+      </div>
+      <div className="text-center">
+        {get(certificate, "additionalData.certSignatories[1].position")}
+      </div>
+    </div>
+    <div className="col-1" />
+  </div>
+);
 
 // eslint-disable-next-line react/display-name
 const Template = certificate => (
@@ -41,98 +85,13 @@ const Template = certificate => (
       className="container"
       style={{ border: 5, borderColor: "#AAA", borderStyle: "solid" }}
     >
-      <div className="row d-flex justify-content-center">
-        <div className="col-2" />
-        <div className="col-8">
-          <img style={fullWidthStyle} src={IMG_LOGO_NP} />
-        </div>
-        <div className="col-2" />
-      </div>
-	  <div
-          className="row d-flex justify-content-center"
-          style={{ marginTop: "2rem" }}
-        >
-          <p style={singaporeTextStyle}>SINGAPORE</p>
-        </div>
-      <div
-        className="row d-flex justify-content-center"
-        style={{ marginTop: "3rem" }}
-      >
-        <p style={printTextStyle}>This is to certify that</p>
-      </div>
-      <div className="row d-flex justify-content-center">
-        <p style={nameTextStyle}>{certificate.recipient.name}</p>
-      </div>
-      <div className="row d-flex justify-content-center">
-        <p style={printTextStyle}>
-          was awarded the
-        </p>
-      </div>
-      <div className="row d-flex justify-content-center">
-        <p style={titleTextStyle}>
-          {" "}
-          Diploma Plus Certificate
-          <br />
-          in
-          <br />
-          {certificate.name}
-        </p>
-      </div>
-      <div
-          className="row d-flex justify-content-center align-items-end"
-          style={{ marginTop: "8rem", marginBottom: "3rem" }}
-        >
-          <div className="col-1"></div> 
-		  <div className="col-4">
-            <div className="px-4">
-              <img
-                style={fullWidthStyle}
-                src={certificate.additionalData.certSignatories[0].signature}
-              />
-              <hr />
-            </div>
-            <div className="text-center">
-              {certificate.additionalData.certSignatories[0].name}
-            </div>
-            <div className="text-center">
-              {certificate.additionalData.certSignatories[0].position}
-            </div>
-          </div>
-		  <div className="col-2"></div> 
-          <div className="col-4">
-            <div className="px-4">
-              <img
-                style={fullWidthStyle}
-                src={certificate.additionalData.certSignatories[1].signature}
-              />
-              <hr />
-            </div>
-            <div className="text-center">
-              {certificate.additionalData.certSignatories[1].name}
-            </div>
-            <div className="text-center">
-              {certificate.additionalData.certSignatories[1].position}
-            </div>
-          </div>
-		  <div className="col-1"></div> 
-        </div>
-		<div>
-			<div>
-			  <p>
-				{formatDatePrefix(certificate.issuedOn)}{" "}
-				{formatDate(certificate.issuedOn)}
-			  </p>
-			</div>
-		</div>
+      {renderLogoNP()}
+      {renderSingapore()}
+      {renderAwardText(certificate)}
+      {renderSignatures(certificate)}
+      {renderIssuingDate(certificate)}
     </div>
-    <div className="container">
-       <div className="row d-flex justify-content-center">
-        <div className="col-6 text-left">
-          {certificate.additionalData.additionalCertId}
-        </div>
-        <div className="col-6 text-right">{formatCertID(certificate.id)}</div>
-      </div>
-    </div>
+    {renderFooter(certificate)}
   </div>
 );
 
--- src/components/CertificateTemplates/NgeeAnnPolytechnic/NP-AA2018-OPTION/certificate.js
+++ src/components/CertificateTemplates/NgeeAnnPolytechnic/NP-AA2018-OPTION/certificate.js
@@ -1,27 +1,11 @@
-import { IMG_LOGO_NP } from "../common";
-
-const fullWidthStyle = {
-  width: "100%",
-  height: "auto"
-};
-
-const printTextStyle = {
-  fontWeight: "500!important",
-  fontWize: "1.2rem",
-  color: "#555",
-  textAlign: "center"
-};
-
-const nameTextStyle = {
-  fontSize: "3rem",
-  textAlign: "center"
-};
-
-const titleTextStyle = {
-  color: "rgb(30,93,200)",
-  fontSize: "3rem",
-  textAlign: "center"
-};
+import {
+  renderLogoNP,
+  renderFooter,
+  fullWidthStyle,
+  printTextStyle,
+  nameTextStyle,
+  titleTextStyle
+} from "../common/certificate";
 
 export const formatDate = dateString => {
   if (!dateString) return null;
@@ -72,11 +56,77 @@ export const formatCertName = (certName, meritFlag) => {
   );
 };
 
-export const formatCertID = certId => {
-  if (!certId) return null;
-  const arrayCertId = certId.split(":");
-  return arrayCertId.length > 0 ? arrayCertId[0] : null;
-};
+const renderAwardText = certificate => (
+  <div>
+    <div
+      className="row d-flex justify-content-center"
+      style={{ marginTop: "3rem" }}
+    >
+      <p style={printTextStyle}>This is to certify that</p>
+    </div>
+    <div className="row d-flex justify-content-center">
+      <p style={nameTextStyle}>{certificate.recipient.name}</p>
+    </div>
+    <div className="row d-flex justify-content-center">
+      <p style={printTextStyle}>
+        has fulfilled {certificate.additionalData.optionType} in
+      </p>
+    </div>
+    <div className="row d-flex justify-content-center">
+      <p style={printTextStyle}>
+        <p style={titleTextStyle}>{certificate.additionalData.optionName}</p>
+      </p>
+    </div>
+    <div className="row d-flex justify-content-center">
+      <p style={printTextStyle}>as part of the course of study in the</p>
+    </div>
+    <div className="row d-flex justify-content-center">
+      <p style={titleTextStyle}>
+        {" "}
+        Diploma
+        <br />
+        in
+        <br />
+        {certificate.name}
+      </p>
+    </div>
+  </div>
+);
+
+const renderSignatures = certificate => (
+  <div
+    className="row d-flex justify-content-center align-items-end"
+    style={{ marginTop: "8rem", marginBottom: "2rem" }}
+  >
+    <div className="col-1" />
+    <div className="col-5">
+      <div className="px-5">
+        <div className="text-center">
+          {formatDate(certificate.issuedOn)}
+          <hr />
+        </div>
+      </div>
+      <div className="text-center">Date</div>
+      <div className="text-center">&nbsp;</div>
+    </div>
+    <div className="col-5">
+      <div className="px-5">
+        <img
+          style={fullWidthStyle}
+          src={certificate.additionalData.certSignatories[0].signature}
+        />
+        <hr />
+      </div>
+      <div className="text-center">
+        {certificate.additionalData.certSignatories[0].name}
+      </div>
+      <div className="text-center">
+        {certificate.additionalData.certSignatories[0].position}
+      </div>
+    </div>
+    <div className="col-1" />
+  </div>
+);
 
 const Template = certificate => (
   <div>
@@ -84,87 +134,11 @@ const Template = certificate => (
       className="container"
       style={{ border: 5, borderColor: "#AAA", borderStyle: "solid" }}
     >
-      <div className="row d-flex justify-content-center">
-        <div className="col-2" />
-        <div className="col-8">
-          <img style={fullWidthStyle} src={IMG_LOGO_NP} />
-        </div>
-        <div className="col-2" />
-      </div>
-      <div
-        className="row d-flex justify-content-center"
-        style={{ marginTop: "3rem" }}
-      >
-        <p style={printTextStyle}>This is to certify that</p>
-      </div>
-      <div className="row d-flex justify-content-center">
-        <p style={nameTextStyle}>{certificate.recipient.name}</p>
-      </div>
-      <div className="row d-flex justify-content-center">
-        <p style={printTextStyle}>
-          has fulfilled {certificate.additionalData.optionType} in
-        </p>
-      </div>
-      <div className="row d-flex justify-content-center">
-        <p style={printTextStyle}>
-          <p style={titleTextStyle}>{certificate.additionalData.optionName}</p>
-        </p>
-      </div>
-      <div className="row d-flex justify-content-center">
-        <p style={printTextStyle}>as part of the course of study in the</p>
-      </div>
-      <div className="row d-flex justify-content-center">
-        <p style={titleTextStyle}>
-          {" "}
-          Diploma
-          <br />
-          in
-          <br />
-          {certificate.name}
-        </p>
-      </div>
-      <div
-        className="row d-flex justify-content-center align-items-end"
-        style={{ marginTop: "8rem", marginBottom: "2rem" }}
-      >
-        <div className="col-1" />
-        <div className="col-5">
-          <div className="px-5">
-            <div className="text-center">
-				{formatDate(certificate.issuedOn)}
-				<hr />
-			</div>
-          </div>
-		  <div className="text-center">
-            Date
-          </div>
-          <div className="text-center">
-            &nbsp;
-          </div>
-        </div>
-        <div className="col-5">
-          <div className="px-5">
-            <img
-              style={fullWidthStyle}
-              src={certificate.additionalData.certSignatories[0].signature}
-            />
-            <hr />
-          </div>
-          <div className="text-center">
-            {certificate.additionalData.certSignatories[0].name}
-          </div>
-          <div className="text-center">
-            {certificate.additionalData.certSignatories[0].position}
-          </div>
-        </div>
-        <div className="col-1" />
-      </div>
+      {renderLogoNP()}
+      {renderAwardText(certificate)}
+      {renderSignatures(certificate)}
     </div>
-    <div className="container">
-      <div className="row d-flex justify-content-center">
-        <div className="col-12 text-right">{formatCertID(certificate.id)}</div>
-      </div>
-    </div>
+    {renderFooter(certificate)}
   </div>
 );
 
