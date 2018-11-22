8a20857db76cd2870bf2fbd69a304f8a768e3793
rjchow <me@rjchow.com>
2018-11-22 09:25:06 +0000

    refactored tabs to use external react-tabs package

--- src/components/MultiCertificateRenderer.js
+++ src/components/MultiCertificateRenderer.js
@@ -1,67 +1,36 @@
 import PropTypes from "prop-types";
+import { Tabs, TabList, Tab, TabPanel } from "react-tabs";
 import styles from "./certificateViewer.scss";
 
-const renderTabList = (templates = []) => {
-  const tabs = templates.map((t, i) => (
-    <li className={`nav-item${i === 0 ? " ml-auto" : ""}`} key={i}>
-      <a
-        className={`slanted-tab ${i === 0 ? " active" : ""}`}
-        data-toggle="tab"
-        aria-controls={`${t.id}`}
-        href={`#${t.id}`}
-        role="tab"
-        id={`${t.id}-tab`}
-      >
-        {t.label}
-      </a>
-    </li>
-  ));
-  return (
-    <ul className="nav nav-tabs" role="tablist">
-      {tabs}
-    </ul>
-  );
-};
-
-const renderTabContent = (certificate, templates = []) => {
-  const contents = templates.map((t, i) => (
-    <div
-      id={`${t.id}`}
-      key={i}
-      className={`tab-pane fade${i === 0 ? " show active" : ""}`}
-      role="tabpanel"
-      aria-labelledby={`${t.id}-tab`}
-    >
-      {t.template(certificate)}
-    </div>
-  ));
-  return (
-    <div className="tab-content bg-white p-3 mt-3 rounded" id="myTabContent">
-      {contents}
-    </div>
-  );
-};
-
 const MultiCertificateRenderer = ({ certificate, templates }) => {
-  const renderedTabs = (
-    <div id={styles["header-ui"]}>
-      <div className={styles["header-container"]}>
-        {renderTabList(templates)}
-      </div>
-    </div>
-  );
-  const renderedContent = renderTabContent(certificate, templates);
   return (
     <div>
-      {renderedTabs}
-      {renderedContent}
+      <Tabs selectedTabClassName="active">
+        <div id={styles["header-ui"]}>
+          <div className={styles["header-container"]}>
+            <TabList className="nav nav-tabs justify-content-end">
+              {templates.map(template => (
+                <Tab className="nav-item slanted-tab">{template.label}</Tab>
+              ))}
+            </TabList>
+          </div>
+        </div>
+        <div
+          className="tab-content bg-white p-3 mt-3 rounded"
+          id="myTabContent"
+        >
+          {templates.map(template => (
+            <TabPanel>{template.template(certificate)}</TabPanel>
+          ))}
+        </div>
+      </Tabs>
     </div>
   );
 };
-
-export default MultiCertificateRenderer;
 
 MultiCertificateRenderer.propTypes = {
   certificate: PropTypes.object,
   templates: PropTypes.array
 };
+
+export default MultiCertificateRenderer;
