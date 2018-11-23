086597387c56e109a12a1d61ecbfd5ff8e4e47d2
angelw22 <angelwong9510@gmail.com>
2018-11-23 11:10:47 +0800

    Landing page styling (#45)
    * Update landing page visuals and responsiveness
    
    * Update landing page visuals and responsiveness
    
    * update logo and dropzone illustration
    
    * remove underline on hover for footer
    
    * update dropzone text and size
    update partner logo sizes
    
    * renamed scss

--- src/components/HomePageContent/AboutSection.js
+++ src/components/HomePageContent/AboutSection.js
@@ -1,51 +1,95 @@
-import css from "./aboutsection.scss";
+import css from "./aboutSection.scss";
 import images from "./AboutImages";
 
 const BenefitsSection = () => (
   <div className={css.section}>
-    <div className={css["flex-container"]}>
-      <h3 className="col-md-3">What we can help you do</h3>
-      <div className="col-md-3">
+    <h3>What we can help you do</h3>
+    <div className={css.container}>
+      <div className="col-lg-4 col-sm-12">
         <div className={css.benefits}>
           {images.valid()}
-          <p>Easy way to view your certificate</p>
+          <div className={css["benefit-text"]}>
+            <h4 className={css["benefit-header"]}>View</h4>
+            <p>Easy way to view your certificate</p>
+          </div>
         </div>
       </div>
-      <div className="col-md-3">
+      <div className="col-lg-4 col-sm-12">
         <div className={css.benefits}>
           {images.genuine()}
-          <p>Make sure it has not been tampered with</p>
+          <div className={css["benefit-text"]}>
+            <h4 className={css["benefit-header"]}>Ensure</h4>
+            <p>Make sure it has not been tampered with</p>
+          </div>
         </div>
       </div>
-      <div className="col-md-3">
+      <div className="col-lg-4 col-sm-12">
         <div className={css.benefits}>
           {images.institution()}
-          <p>Check if it has been issued by the right institution</p>
+          <div className={css["benefit-text"]}>
+            <h4 className={css["benefit-header"]}>Verify</h4>
+            <p>Check if it has been issued by the right institution</p>
+          </div>
         </div>
       </div>
     </div>
   </div>
 );
 
-const HowItWorks = () => (
-  <div className={css.section}>
-    <h3>How it works</h3>
-    <div className={css.howitworks}>
-      <img src="/static/images/aboutsection/howitworksgraphic.png" />
+const HowItWorks = () => {
+  const sections = [
+    {
+      key: "onetwo",
+      text:
+        "When an OpenCerts certificate is created, a unique digital signature, or unique identifier, is tagged to it. Under this, the information from the certificate is stored on the blockchain."
+    },
+    // {
+    //   key: "one",
+    //   text: "When an OpenCerts certificate is created, a unique digital signature, or unique identifier, is tagged to it."
+    // },
+    // {
+    //   key: "two",
+    //   text: "Under the same digital signature, the information from the certificate is stored on the blockchain."
+    // },
+    {
+      key: "three",
+      text:
+        "When you open the .opencert file on this site, its contents will be compared with what was stored in the blockchain."
+    },
+    {
+      key: "four",
+      text:
+        "If the information is accurate and true, you will be able to view the certificate."
+    }
+  ];
+
+  const section = sections.map((item, i) => (
+    <div key={i} id={css[item.key]}>
+      {images[item.key]()}
+      <p>{item.text}</p>
     </div>
-  </div>
-);
+  ));
+
+  return (
+    <div className={css.howitworks}>
+      <div className={css["hiw-container"]}>{section}</div>
+    </div>
+  );
+};
 
 const AboutSection = () => (
   <div>
-    <div className="row p-4 bg-white" id="benefits">
+    <div className="row bg-light" id={css.benefits}>
       <div className={css.main}>
         <BenefitsSection />
       </div>
     </div>
-    <div className="row p-4 bg-light" id="how-it-works">
+    <div className="row bg-light" id="how-it-works">
       <div className={css.main}>
-        <HowItWorks />
+        <div className={css.section}>
+          <h3>How it works</h3>
+          <HowItWorks />
+        </div>
       </div>
     </div>
   </div>
