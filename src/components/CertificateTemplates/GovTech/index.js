7e29042149ac401b7b4cb492b705be403ba30dd5
yehjxraymond <yehjxraymond@gmail.com>
2018-11-30 13:38:42 +0800

    Feature/govtech cert (#49)
    * Basic template
    
    * Added template for govtech cert
    
    * Refactored styles
    
    * Removed static resources
    
    * Fixed static asset path
    
    * Added signature

---
+++ src/components/CertificateTemplates/GovTech/index.js
@@ -0,0 +1,11 @@
+import Certificate from "./certificate";
+
+const templates = [
+  {
+    id: "certificate",
+    label: "Certificate",
+    template: Certificate
+  }
+];
+
+export default { templates };
