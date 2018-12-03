1d704bd93dc3a1f5135e891bbdadc3aeb4898368
NCS\P1201675 <erwin.sg@ncs.com.sg>
2018-12-03 21:30:53 +0800

    NCS: eCert Template for SIT

--- src/components/CertificateTemplates/index.js
+++ src/components/CertificateTemplates/index.js
@@ -5,6 +5,7 @@ import NPAA2018BMSCLT from "./NgeeAnnPolytechnic/NP-AA2018-BMS(CLT)";
 import NPAA2018ECH from "./NgeeAnnPolytechnic/NP-AA2018-ECH";
 import NPAA2018LDH from "./NgeeAnnPolytechnic/NP-AA2018-LDH";
 import NPAA2018PHARM from "./NgeeAnnPolytechnic/NP-AA2018-PHARM";
+import SITCerts from "./SITCerts";
 
 const templates = {
   default: DefaultCert,
@@ -13,7 +14,8 @@ const templates = {
   "NP-AA2018-BMS(CLT)": NPAA2018BMSCLT,
   "NP-AA2018-ECH": NPAA2018ECH,
   "NP-AA2018-LDH": NPAA2018LDH,
-  "NP-AA2018-PHARM": NPAA2018PHARM
+  "NP-AA2018-PHARM": NPAA2018PHARM,
+  "SITCerts": SITCerts
 };
 
 export default templates;
