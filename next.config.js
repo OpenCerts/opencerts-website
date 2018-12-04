15a48aa8994126ff85711e1b4bff548a07280c2b
RJ <me@rjchow.com>
2018-12-04 14:28:34 +0800

    wip

--- next.config.js
+++ next.config.js
@@ -1,18 +1,38 @@
 const withSass = require("@zeit/next-sass");
 
-module.exports = withSass({
-  cssModules: true,
-  exportPathMap: function exportMap() {
-    return {
-      "/": { page: "/" },
-      "/registry": { page: "/registry" },
-      "/privacy": { page: "/privacy" },
-      "/viewer": { page: "/viewer" },
-      "/faq": { page: "/faq" }
-    };
-  },
-  // Variables passed to both server and client
-  publicRuntimeConfig: {
-    network: process.env.NET
-  }
-});
+module.exports = (phase, defaultConfig) => {
+  console.log("config", defaultConfig);
+  return withSass({
+    // webpack: function(config, { isServer }) {
+    //   console.log(
+    //     "webpack",
+    //     isServer,
+    //     config.optimization.splitChunks.cacheGroups
+    //   );
+    //   if (!isServer) {
+    //     config.optimization.splitChunks.cacheGroups.templates = {
+    //       name: "templates",
+    //       test: /src\/components\/CertificateTemplates/,
+          
+    //       chunks: "all",
+    //       enforce: true
+    //     };
+    //   }
+    //   return config;
+    // },
+    cssModules: true,
+    exportPathMap: function exportMap() {
+      return {
+        "/": { page: "/" },
+        "/registry": { page: "/registry" },
+        "/privacy": { page: "/privacy" },
+        "/viewer": { page: "/viewer" },
+        "/faq": { page: "/faq" }
+      };
+    },
+    // Variables passed to both server and client
+    publicRuntimeConfig: {
+      network: process.env.NET
+    }
+  });
+};
