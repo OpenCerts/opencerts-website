dce0a56b4c6a71d70e8019d30e638731c82cca8b
Raymond Yeh <raymond_yeh@tech.gov.sg>
2018-12-05 14:03:39 +0800

    Combined Meta with _document

--- pages/faq.js
+++ pages/faq.js
@@ -1,12 +1,14 @@
 import { connect } from "react-redux";
-import Meta from "../src/components/Layout/Meta";
+import Head from "next/head";
 import NavigationBar from "../src/components/Layout/NavigationBar";
 import FooterBar from "../src/components/Layout/FooterBar";
 import FaqContent from "../src/components/FAQ/FaqContent";
 
 const FaqPage = () => (
   <div>
-    <Meta />
+    <Head>
+      <title>OpenCerts - FAQ</title>
+    </Head>
     <NavigationBar active="faq" />
     <FaqContent />
     <FooterBar />
