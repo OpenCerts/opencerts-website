840ca60416cb18924804a0cc9122262f9ca608fa
Raymond Yeh <raymond_yeh@tech.gov.sg>
2018-12-05 14:50:28 +0800

    Updated registry with govtech and NP details

--- src/components/RegistryPageContainer.js
+++ src/components/RegistryPageContainer.js
@@ -1,31 +1,17 @@
 const members = [
   {
-    name: "Issuer A",
-    address: "0x9999999999999999999999999999999",
-    website: "https://tech.gov.sg",
-    email: "issuer@opencerts.io",
-    phone: "+65 6666 6666"
+    name: "Government Technology Agency of Singapore (GovTech)",
+    address: "0x007d40224f6562461633ccfbaffd359ebb2fc9ba",
+    website: "https://www.tech.gov.sg",
+    email: "info@tech.gov.sg",
+    phone: "6211 2100"
   },
   {
-    name: "Issuer B",
-    address: "0x9999999999999999999999999999999",
-    website: "https://tech.gov.sg",
-    email: "issuer@opencerts.io",
-    phone: "+65 6666 6666"
-  },
-  {
-    name: "Issuer C",
-    address: "0x9999999999999999999999999999999",
-    website: "https://tech.gov.sg",
-    email: "issuer@opencerts.io",
-    phone: "+65 6666 6666"
-  },
-  {
-    name: "Issuer D",
-    address: "0x9999999999999999999999999999999",
-    website: "https://tech.gov.sg",
-    email: "issuer@opencerts.io",
-    phone: "+65 6666 6666"
+    name: "Ngee Ann Polytechnic",
+    address: "0xa5d801265D29A6F1015a641BfC0e39Ee3dA2AC76",
+    website: "https://www.np.edu.sg",
+    email: "asknp@np.edu.sg",
+    phone: "6466 6555"
   }
 ];
 
