4ad792374388876fb08fa5e9604c97d893b803eb
NCS\P1201675 <erwin.sg@ncs.com.sg>
2018-12-11 10:23:10 +0800

    NCS: eCert Template for SIT_20181210_4

--- src/components/CertificateTemplates/SITCerts/index.js
+++ src/components/CertificateTemplates/SITCerts/index.js
@@ -8,6 +8,6 @@ const templates = [
   }
 ];
 
-const addresses = ["0x897E224a6a8b72535D67940B3B8CE53f9B596800"];
+const addresses = ["0x536EdEd27ae895F503E64Af877ee742B7bBC1ea2"];
 
 export default { templates, addresses };
