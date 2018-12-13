1e8e23936b4e25456e4eda0b1a12d036e9b96d22
RJ <me@rjchow.com>
2018-12-13 11:25:41 +0800

    wip

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,11 @@
 module.exports = {
   setupFiles: ["<rootDir>/jest.setup.js"],
   testPathIgnorePatterns: ["<rootDir>/.next/", "<rootDir>/node_modules/"],
-  collectCoverage: true,
+  transform: {  // https://github.com/zeit/next.js/issues/5416#issuecomment-441589662
+    "^.+\\.js$": "babel-jest",
+    ".+\\.(css|styl|less|sass|scss)$": "jest-transform-css"
+  },
+  collectCoverage: false,
   coverageDirectory: "<rootDir>/.coverage/",
-  "collectCoverageFrom": [
-    "src/**/*.{js,jsx}"
-  ]
+  collectCoverageFrom: ["src/**/*.{js,jsx}"]
 };
