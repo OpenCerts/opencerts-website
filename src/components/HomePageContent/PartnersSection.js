1649e004a82def0bbf85aee1a31559467bcf89b3
Raymond Yeh <yehjxraymond@gmail.com>
2019-01-16 14:02:14 +0800

    Added ssg logo

--- src/components/HomePageContent/PartnersSection.js
+++ src/components/HomePageContent/PartnersSection.js
@@ -7,10 +7,6 @@ const CarouselSection = () => {
       value: "/static/images/NP_logo.svg"
     },
     {
-      key: "TP",
-      value: "/static/images/TP_logo.svg"
-    },
-    {
       key: "NYP",
       value: "/static/images/NYP_logo.svg"
     },
@@ -23,40 +19,52 @@ const CarouselSection = () => {
       value: "/static/images/SP_logo.svg"
     },
     {
-      key: "NUS",
-      value: "/static/images/NUS_logo.png"
-    },
-    {
-      key: "NTU",
-      value: "/static/images/NTU_logo.png"
-    },
-    {
-      key: "SIM",
-      value: "/static/images/SIM_logo.png"
-    },
-    {
-      key: "SUTD",
-      value: "/static/images/SUTD_logo.png"
-    },
-    {
-      key: "SUSS",
-      value: "/static/images/SUSS_logo.png"
-    },
-    {
-      key: "SIT",
-      value: "/static/images/SIT_logo.png"
-    },
-    {
-      key: "SEAB",
-      value: "/static/images/SEAB_logo.png"
+      key: "TP",
+      value: "/static/images/TP_logo.svg"
     },
     {
       key: "ITE",
       value: "/static/images/ITE_logo.png"
     },
     {
+      key: "NTU",
+      value: "/static/images/NTU_logo.png"
+    },
+    {
+      key: "NUS",
+      value: "/static/images/NUS_logo.png"
+    },
+    {
+      key: "SIM",
+      value: "/static/images/SIM_logo.png"
+    },
+    {
+      key: "SIT",
+      value: "/static/images/SIT_logo.png"
+    },
+    {
+      key: "SMU",
+      value: "/static/images/SMU_logo.png"
+    },
+    {
+      key: "SUSS",
+      value: "/static/images/SUSS_logo.png"
+    },
+    {
+      key: "SUTD",
+      value: "/static/images/SUTD_logo.png"
+    },
+    {
+      key: "SEAB",
+      value: "/static/images/SEAB_logo.png"
+    },
+    {
       key: "CSC",
       value: "/static/images/CSC_logo.png"
+    },
+    {
+      key: "SSG",
+      value: "/static/images/SSG_logo.png"
     },
     {
       key: "govtech",
