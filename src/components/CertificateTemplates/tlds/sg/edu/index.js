f47fa60530e93adee984026f8377106d12ad89f9
LaurenceZhang <zhanglaurence@hotmail.com>
2019-02-26 17:36:11 +0800

    Update index.js

--- src/components/CertificateTemplates/tlds/sg/edu/index.js
+++ src/components/CertificateTemplates/tlds/sg/edu/index.js
@@ -2,6 +2,6 @@ import { addDirToTemplatePath } from "template-utils/addDirToTemplatePath";
 
 import singaporetech from "./singaporetech";
 import np from "./np";
-import np from "./ibs";
+import ibs from "./ibs";
 
 export default addDirToTemplatePath("edu", { ...singaporetech, ...np, ...ibs });
