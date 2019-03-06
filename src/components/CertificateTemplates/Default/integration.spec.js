0a90483b47441fa790c2d16bf552143518dc6a5c
Raymond Yeh <yehjxraymond@gmail.com>
2019-03-06 17:22:47 +0800

    WIP

---
+++ src/components/CertificateTemplates/Default/integration.spec.js
@@ -0,0 +1,53 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { Selector } from "testcafe";
+
+// eslint-disable-next-line
+fixture("Default Certificate Template").page`http://localhost:3000`;
+
+const Certificate = "./DEFAULT_CERTIFICATE.json";
+
+const ButtonViewCertificateAnyway = Selector("#certificate-view-anyway");
+const CertificateVerifyBlock = Selector("#certificate-verify-block");
+const ButtonPrint = Selector("#btn-print");
+const ButtonEmail = Selector("#btn-email");
+const ButtonDownload = Selector("#btn-download");
+const ButtonViewAnother = Selector("#btn-view-another");
+const TabListTemplate = Selector("#template-tabs-list");
+const BannerPrivacyFilter = Selector("#banner-privacy-filter");
+const RenderedCertificate = Selector("#rendered-certificate");
+
+test("Default certificate is rendered correctly", async t => {
+  // Uploads certificate via dropzone
+  await t.setFilesToUpload("input[type=file]", [Certificate]);
+  await t.click(ButtonViewCertificateAnyway);
+
+  // Validates rendering of the certificate
+  // Header items rendered
+  await t
+    .expect(CertificateVerifyBlock.textContent)
+    .contains("Unknown Institution");
+  await t.expect(ButtonPrint.visible).ok();
+  await t.expect(ButtonEmail.visible).ok();
+  await t.expect(ButtonDownload.visible).ok();
+  await t.expect(ButtonViewAnother.visible).ok();
+
+  // Certificate tabs rendered
+  await t.expect(TabListTemplate.textContent).contains("Certificate");
+
+  // Privacy filter notice rendered
+  await t.expect(BannerPrivacyFilter.visible).ok();
+  await t
+    .expect(BannerPrivacyFilter.textContent)
+    .contains("OpenCerts Privacy Filter Enabled");
+
+  // Certificate rendered
+  await t.expect(RenderedCertificate.visible).ok();
+  await t
+    .expect(RenderedCertificate.textContent)
+    .contains("Sample Certificate");
+  await t
+    .expect(RenderedCertificate.textContent)
+    .contains("A_SAMPLE_CERTIFICATE");
+  await t.expect(RenderedCertificate.textContent).contains("Issuer Info");
+  await t.expect(RenderedCertificate.textContent).contains("Transcript");
+});
--- src/components/CertificateTemplates/tlds/sg/edu/np/NP-AA2018-BMS(CLT)/integration.spec.js
+++ src/components/CertificateTemplates/tlds/sg/edu/np/NP-AA2018-BMS(CLT)/integration.spec.js
@@ -2,39 +2,41 @@
 import { Selector } from "testcafe";
 
 // eslint-disable-next-line
-fixture`Getting Started`.page`http://localhost:3000`;
+fixture("Default Certificate Template").page`http://localhost:3000`;
+
+const Certificate = "./NP_Certs_FT_BMS.opencert";
 
 const ButtonViewCertificateAnyway = Selector("#certificate-view-anyway");
-const CertificateVerifyBlock = Selector("#certificate-verify-block");
-const ButtonPrint = Selector("#btn-print");
-const ButtonEmail = Selector("#btn-email");
-const ButtonDownload = Selector("#btn-download");
-const ButtonViewAnother = Selector("#btn-view-another");
 const TabListTemplate = Selector("#template-tabs-list");
-const BannerPrivacyFilter = Selector("#banner-privacy-filter");
 const RenderedCertificate = Selector("#rendered-certificate");
 
-test("Certificate viewer is rendered correctly ", async t => {
+test.only("Default certificate is rendered correctly", async t => {
   // Uploads certificate via dropzone
-  await t.setFilesToUpload("input[type=file]", [
-    "./fixtures/DEFAULT_CERTIFICATE.json"
-  ]);
-  await t.click(ButtonViewCertificateAnyway);
-
-  // Validates rendering of the certificate
-  // Header items rendered
-  await t
-    .expect(CertificateVerifyBlock.textContent)
-    .contains("Unknown Institution");
-  await t.expect(ButtonPrint.visible).ok();
-  await t.expect(ButtonEmail.visible).ok();
-  await t.expect(ButtonDownload.visible).ok();
-  await t.expect(ButtonViewAnother.visible).ok();
+  await t.setFilesToUpload("input[type=file]", [Certificate]);
 
   // Certificate tabs rendered
   await t.expect(TabListTemplate.textContent).contains("Certificate");
+  await t.expect(TabListTemplate.textContent).contains("Transcript");
 
-  // Privacy filter notice rendered
+  // Certificate tab content
+  await t
+    .expect(RenderedCertificate.textContent)
+    .contains("Student Name BMS Cert");
+  await t
+    .expect(RenderedCertificate.textContent)
+    .contains("Diploma with Merit");
+  await t
+    .expect(RenderedCertificate.textContent)
+    .contains("Biomedical Science");
+  await t.expect(RenderedCertificate.textContent).contains("Principal");
+  await t.expect(RenderedCertificate.textContent).contains("Council Chairman");
+  await t
+    .expect(RenderedCertificate.textContent)
+    .contains("Chief Executive Officer");
+  await t
+    .expect(RenderedCertificate.textContent)
+    .contains("National University Hospital");
+  /*
   await t.expect(BannerPrivacyFilter.visible).ok();
   await t
     .expect(BannerPrivacyFilter.textContent)
@@ -50,4 +52,6 @@ test("Certificate viewer is rendered correctly ", async t => {
     .contains("A_SAMPLE_CERTIFICATE");
   await t.expect(RenderedCertificate.textContent).contains("Issuer Info");
   await t.expect(RenderedCertificate.textContent).contains("Transcript");
+  */
+  await t.wait(10000);
 });
