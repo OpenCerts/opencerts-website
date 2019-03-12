fda782adb1578fe625177c3d189d49eb83afd1d5
Sumit Chaudhari <sumit.nitsurat@gmail.com>
2019-03-12 15:38:18 +0800

    [#116] - feat/smulogo
    - Added niec logo
    - Seprated the images constant into new file.
    - Modifed css for niec logo

--- src/components/HomePageContent/PartnersSection.js
+++ src/components/HomePageContent/PartnersSection.js
@@ -1,77 +1,8 @@
 import css from "./partnersPage.scss";
+import { IMAGES } from "../../constants/PartnerLogo";
 
 const CarouselSection = () => {
-  const images = [
-    {
-      key: "NP",
-      value: "/static/images/NP_logo.svg"
-    },
-    {
-      key: "NYP",
-      value: "/static/images/NYP_logo.png"
-    },
-    {
-      key: "RP",
-      value: "/static/images/RP_logo.svg"
-    },
-    {
-      key: "SP",
-      value: "/static/images/SP_logo.svg"
-    },
-    {
-      key: "TP",
-      value: "/static/images/TP_logo.svg"
-    },
-    {
-      key: "SEAB",
-      value: "/static/images/SEAB_logo.png"
-    },
-    {
-      key: "ITE",
-      value: "/static/images/ITE_logo.png"
-    },
-    {
-      key: "NTU",
-      value: "/static/images/NTU_logo.png"
-    },
-    {
-      key: "NUS",
-      value: "/static/images/NUS_logo.png"
-    },
-    {
-      key: "SIM",
-      value: "/static/images/SIM_logo.png"
-    },
-    {
-      key: "SIT",
-      value: "/static/images/SIT_logo.png"
-    },
-    {
-      key: "SMU",
-      value: "/static/images/SMU_logo.svg"
-    },
-    {
-      key: "SUSS",
-      value: "/static/images/SUSS_logo.png"
-    },
-    {
-      key: "SUTD",
-      value: "/static/images/SUTD_logo.png"
-    },
-    {
-      key: "CSC",
-      value: "/static/images/CSC_logo.png"
-    },
-    {
-      key: "SSG",
-      value: "/static/images/SSG_logo.png"
-    },
-    {
-      key: "govtech",
-      value: "/static/images/GOVTECH_logo.png"
-    }
-  ];
-  const items = images.map((item, i) => (
+  const items = IMAGES.map((item, i) => (
     <div className={css["logo-container"]} key={i}>
       <img className="mx-auto" src={item.value} id={css[item.key]} />
     </div>
