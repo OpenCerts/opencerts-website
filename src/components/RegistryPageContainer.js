9f9b506bf7cb7bf7054fecd84dd88179e05f4109
Sumit Chaudhari <sumit.nitsurat@gmail.com>
2019-04-02 15:49:58 +0800

    [#139] - Fix registry Page
    - Added styling and mobile responsive for registry page.

--- src/components/RegistryPageContainer.js
+++ src/components/RegistryPageContainer.js
@@ -1,72 +1,75 @@
+import css from "./registry.scss";
+
 const members = [
   {
     name: "Government Technology Agency of Singapore (GovTech)",
     address: "0x007d40224f6562461633ccfbaffd359ebb2fc9ba",
     website: "https://www.tech.gov.sg",
     email: "info@tech.gov.sg",
-    phone: "+65 6211 2100"
+    phone: "+65 6211 2100",
+    logo: "/static/images/GOVTECH_logo.png"
   },
   {
     name: "Ngee Ann Polytechnic",
     address: "0xa5d801265D29A6F1015a641BfC0e39Ee3dA2AC76",
     website: "https://www.np.edu.sg",
     email: "asknp@np.edu.sg",
-    phone: "+65 6466 6555"
+    phone: "+65 6466 6555",
+    logo: "/static/images/NP_logo.svg"
   }
 ];
 
 const renderMembers = () =>
   members.map((m, i) => (
-    <div key={i} className="col-6 mb-3">
-      <h4>{m.name}</h4>
-      {m.address ? (
-        <div>
-          Certificate Store:{" "}
-          <a href={`https://etherscan.io/address/${m.address}`}>{m.address}</a>
-        </div>
-      ) : (
-        ""
-      )}
-      {m.website ? (
-        <div>
-          Website:{" "}
-          <a href={m.website} target="_blank" rel="noopener noreferrer">
-            {m.website}
-          </a>
-        </div>
-      ) : (
-        ""
-      )}
-      {m.email ? (
-        <div>
-          Email: <a href={`mailto:${m.email}`}>{m.email}</a>
-        </div>
-      ) : (
-        ""
-      )}
-      {m.phone ? (
-        <div>
-          Phone: <a href={`tel:${m.phone}`}>{m.phone}</a>
-        </div>
-      ) : (
-        ""
-      )}
+    <div key={i} className={`col-lg-6 col-md-12 mb-3 ${css["m-pd-0"]}`}>
+      <div className={css["partner-block"]}>
+        <img className={`${css.logo}`} src={m.logo} />
+        <h4 className={css["partner-name"]}>{m.name}</h4>
+        {m.address ? (
+          <div>
+            Certificate Store:{" "}
+            <a href={`https://etherscan.io/address/${m.address}`}>
+              {m.address}
+            </a>
+          </div>
+        ) : (
+          ""
+        )}
+        {m.website ? (
+          <div>
+            Website:{" "}
+            <a href={m.website} target="_blank" rel="noopener noreferrer">
+              {m.website}
+            </a>
+          </div>
+        ) : (
+          ""
+        )}
+        {m.email ? (
+          <div>
+            Email: <a href={`mailto:${m.email}`}>{m.email}</a>
+          </div>
+        ) : (
+          ""
+        )}
+        {m.phone ? (
+          <div>
+            Phone: <a href={`tel:${m.phone}`}>{m.phone}</a>
+          </div>
+        ) : (
+          ""
+        )}
+      </div>
     </div>
   ));
 
 const RegistryPage = () => (
   <div className="container-fluid">
-    <div
-      className="row bg-brand-dark text-white p-3"
-      style={{ fontFamily: "sans-serif", fontWeight: 100 }}
-    >
-      <h1>Registry</h1>
+    <div className={`row bg-brand-dark text-white ${css["r-section"]}`}>
+      <h1 className="col-12">Registry</h1>
     </div>
-    <div
-      className="row bg-brand-dark text-white"
-      style={{ fontFamily: "sans-serif", fontWeight: 100 }}
-    >
-      <div className="col-6 my-3">
+    <div className={`row bg-brand-dark text-white ${css["r-section"]}`}>
+      <div className="col-lg-6 col-md-12 my-3">
         <p>
           The registry is a list of recognised issuers with their certificate
           store addresses. Certificates from these issuers can be recognised and
@@ -79,7 +82,7 @@ const RegistryPage = () => (
         </p>
       </div>
     </div>
-    <div className="row p-3">{renderMembers()}</div>
+    <div className={`row ${css["r-section"]}`}>{renderMembers()}</div>
   </div>
 );
 
