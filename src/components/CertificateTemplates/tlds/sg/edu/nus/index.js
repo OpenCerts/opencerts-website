41362adf0ca4aa25ca1f5278f628e375a7c469fd
wuzehe <ccewuz@nus.edu.sg>
2019-04-10 09:02:08 +0800

    20190410 code change due to bsync with upstream

--- src/components/CertificateTemplates/tlds/sg/edu/nus/index.js
+++ src/components/CertificateTemplates/tlds/sg/edu/nus/index.js
@@ -1,9 +1,9 @@
-import dynamic from "next/dynamic";
-import { addDirToTemplatePath } from "../../../../utils/addDirToTemplatePath";
+import dynamic from "template-utils/dynamic";
+import { addDirToTemplatePath } from "template-utils/addDirToTemplatePath";
 
-const NUSK12019 = dynamic(import("./NUS-K1-2019"));
+const NUSK12019 = dynamic(() => import("./NUS-K1-2019"));
 
-const NUSTSUGGD2019 = dynamic(import("./NUSTS-UGGD-2019"));
+const NUSTSUGGD2019 = dynamic(() => import("./NUSTS-UGGD-2019"));
 
 const templates = {
   "NUS-K1-2019": NUSK12019,
