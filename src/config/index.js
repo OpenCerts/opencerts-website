b3884bd86dce7bad91a6fffd6de577fbd8f9d1c8
Raymond Yeh <yehjxraymond@gmail.com>
2019-04-12 15:45:30 +0800

    Added ropsten emial api

--- src/config/index.js
+++ src/config/index.js
@@ -15,16 +15,16 @@ export const NETWORK_TYPES = {
 const GA_PRODUCTION_ID = "UA-130492260-1";
 const GA_DEVELOPMENT_ID = "UA-130492260-2";
 
-export const DEFAULT_NETWORK =
-  publicRuntimeConfig.network === "mainnet"
-    ? NETWORK_TYPES.INFURA_MAINNET
-    : NETWORK_TYPES.INFURA_ROPSTEN;
-export const GA_ID =
-  publicRuntimeConfig.network === "mainnet"
-    ? GA_PRODUCTION_ID
-    : GA_DEVELOPMENT_ID;
+const isMainnet = publicRuntimeConfig.network === "mainnet";
+
+export const DEFAULT_NETWORK = isMainnet
+  ? NETWORK_TYPES.INFURA_MAINNET
+  : NETWORK_TYPES.INFURA_ROPSTEN;
+export const GA_ID = isMainnet ? GA_PRODUCTION_ID : GA_DEVELOPMENT_ID;
 export const CAPTCHA_CLIENT_KEY = "6LfiL3EUAAAAAHrfLvl2KhRAcXpanNXDqu6M0CCS";
-export const EMAIL_API_URL = "https://api.opencerts.io/email";
+export const EMAIL_API_URL = isMainnet
+  ? "https://api.opencerts.io/email"
+  : "https://api-ropsten.opencerts.io/email";
 export const INFURA_PROJECT_ID = "1f1ff2b3fca04f8d99f67d465c59e4ef";
 
 trace(`DEFAULT_NETWORK: ${DEFAULT_NETWORK}`);
