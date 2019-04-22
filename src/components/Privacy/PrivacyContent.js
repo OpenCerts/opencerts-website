d22df1ac19829d56e71b5d5396ff409c1aa04464
Jason Kam <thefiend@hotmail.sg>
2019-04-22 14:23:11 +0800

    Added hyperlink to links and email

--- src/components/Privacy/PrivacyContent.js
+++ src/components/Privacy/PrivacyContent.js
@@ -58,7 +58,8 @@ const Policy = () => (
         sites.
       </li>
       <li>
-        Please contact 1800 211 0777 or qsm@tech.gov.sg if you:
+        Please contact 1800 211 0777 or{" "}
+        <a href="mailto:qsm@tech.gov.sg">qsm@tech.gov.sg</a> if you:
         <ol type="a">
           <li>
             have any enquires or feedback on our data protection policies and
