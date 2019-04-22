d7df476d6f096bca7df20673e5c438d2ad7bba42
Jason Kam <thefiend@hotmail.sg>
2019-04-22 15:26:08 +0800

    Capitalised starting characters of line

--- src/components/Privacy/PrivacyContent.js
+++ src/components/Privacy/PrivacyContent.js
@@ -62,11 +62,11 @@ const Policy = () => (
         <a href="mailto:qsm@tech.gov.sg">qsm@tech.gov.sg</a> if you:
         <ol type="a">
           <li>
-            have any enquires or feedback on our data protection policies and
-            procedures,
+            Have any enquires or feedback on our data protection policies and
+            procedures.
           </li>
           <li>
-            need more information on or access to data which you have provided
+            Need more information on or access to data which you have provided
             to us in the past.
           </li>
         </ol>
