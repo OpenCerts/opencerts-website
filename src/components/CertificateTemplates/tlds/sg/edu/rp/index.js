9c8ef66a11e7dd5c0c6d5d0cd0a5120bd7a1a640
RJ <me@rjchow.com>
2019-04-22 13:51:58 +0800

    fixed dynamic imports

--- src/components/CertificateTemplates/tlds/sg/edu/rp/index.js
+++ src/components/CertificateTemplates/tlds/sg/edu/rp/index.js
@@ -1,13 +1,13 @@
 import dynamic from "template-utils/dynamic";
 import { addDirToTemplatePath } from "template-utils/addDirToTemplatePath";
 
-const RPAA2018PMAIN = dynamic(
+const RPAA2018PMAIN = dynamic(() =>
   import("./2018-P-MAIN" /* webpackChunkName: "RPTemplates" */)
 );
-const RPAA2018CDPLUS = dynamic(
+const RPAA2018CDPLUS = dynamic(() =>
   import("./2018-C-DPLUS" /* webpackChunkName: "RPTemplates" */)
 );
-const RPAA2018CMC = dynamic(
+const RPAA2018CMC = dynamic(() =>
   import("./2018-C-MC" /* webpackChunkName: "RPTemplates" */)
 );
 const templates = {
