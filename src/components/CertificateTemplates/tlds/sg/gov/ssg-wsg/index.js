7fe6dc47d6673c7113db86eee0d1a2221e4c08b5
ajeetbhatnagar1 <ajeet.a.bhatnagar@accenture.com>
2019-04-22 20:01:25 +0800

    Added Next 5 Templates for QUAL and SOA

--- src/components/CertificateTemplates/tlds/sg/gov/ssg-wsg/index.js
+++ src/components/CertificateTemplates/tlds/sg/gov/ssg-wsg/index.js
@@ -21,10 +21,28 @@ const FQ006 = dynamic(() =>
   import("./FQ-006" /* webpackChunkName: "SSGTemplates" */)
 );
 
+const SFFQ001 = dynamic(() =>
+  import("./SF_FQ_001" /* webpackChunkName: "SSGTemplates" */)
+);
+
+const SFFQ002 = dynamic(() =>
+  import("./SF_FQ_002" /* webpackChunkName: "SSGTemplates" */)
+);
+
+const SFFQ004 = dynamic(() =>
+  import("./SF_FQ_004" /* webpackChunkName: "SSGTemplates" */)
+);
+
+const QualReprint = dynamic(() =>
+  import("./QUAL_Reprint" /* webpackChunkName: "SSGTemplates" */)
+);
 const SOA001 = dynamic(() =>
   import("./SOA-001" /* webpackChunkName: "SSGTemplates" */)
 );
 
+const SFSOA001 = dynamic(() =>
+  import("./SF_SOA_001" /* webpackChunkName: "SSGTemplates" */)
+);
 const Transcript = dynamic(() =>
   import("./Trans" /* webpackChunkName: "SSGTemplates" */)
 );
@@ -35,7 +53,12 @@ const templates = {
   "FQ-004": FQ004,
   "FQ-005": FQ005,
   "FQ-006": FQ006,
+  SF_FQ_001: SFFQ001,
+  SF_FQ_002: SFFQ002,
+  SF_FQ_004: SFFQ004,
+  QUAL_Reprint: QualReprint,
   "SOA-001": SOA001,
+  SF_SOA_001: SFSOA001,
   Trans: Transcript
 };
 
