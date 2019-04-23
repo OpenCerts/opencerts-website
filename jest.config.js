3d43db2d6a83865594c86172f42bc318f9933044
Raymond Yeh <yehjxraymond@gmail.com>
2019-04-23 17:25:45 +0800

    wip

--- jest.config.js
+++ jest.config.js
@@ -4,9 +4,11 @@ module.exports = {
   collectCoverage: true,
   coverageDirectory: "<rootDir>/.coverage/",
   collectCoverageFrom: ["src/**/*.{js,jsx}"],
-  testMatch: [ "**/__tests__/**/*.[jt]s?(x)", "**/?(*.)test.[jt]s?(x)" ],
+  testMatch: ["**/__tests__/**/*.[jt]s?(x)", "**/?(*.)test.[jt]s?(x)"],
   moduleNameMapper: {
-    "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/_mocks_/fileMock.js",
+    "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
+      "<rootDir>/_mocks_/fileMock.js",
     "\\.(css|sass|scss)$": "<rootDir>/_mocks_/styleMock.js"
-  }
+  },
+  testResultsProcessor: "jest-sonar-reporter"
 };
