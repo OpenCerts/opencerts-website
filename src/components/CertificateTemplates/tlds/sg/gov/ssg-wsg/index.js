025363894ad41a80434244c20f6ff98c869b893d
ajeetbhatnagar1 <ajeet.a.bhatnagar@accenture.com>
2019-04-24 04:21:49 +0800

    Added Templates for SOA(SFSOAHR01,SOAHR01,SOAFB001)

--- src/components/CertificateTemplates/tlds/sg/gov/ssg-wsg/index.js
+++ src/components/CertificateTemplates/tlds/sg/gov/ssg-wsg/index.js
@@ -26,6 +26,15 @@ const SOA002 = dynamic(() =>
 const SOA003 = dynamic(() =>
   import("./SOA-003" /* webpackChunkName: "SSGTemplates" */)
 );
+const SOAHR01 = dynamic(() =>
+  import("./SOA-HR-01" /* webpackChunkName: "SSGTemplates" */)
+);
+const SFSOAHR01 = dynamic(() =>
+  import("./SF_SOA_HR_01" /* webpackChunkName: "SSGTemplates" */)
+);
+const SOAFB001 = dynamic(() =>
+  import("./SOA-FB-001" /* webpackChunkName: "SSGTemplates" */)
+);
 const SOAIT001 = dynamic(() =>
   import("./SOA-IT-001" /* webpackChunkName: "SSGTemplates" */)
 );
@@ -43,6 +52,19 @@ const templates = {
   "SOA-001": SOA001,
   "SOA-002": SOA002,
   "SOA-003": SOA003,
+  "SOA-HR-01": SOAHR01,
+  "SOA-FB-001": SOAFB001,
+
+  SF_SOA_FB_001: SOAFB001,
+  "SOA-HR-02": SOAHR01,
+  "SOA-HR-03": SOAHR01,
+  "SOA-HR-04": SOAHR01,
+  "SOA-HR-05": SOAHR01,
+  SF_SOA_HR_01: SFSOAHR01,
+  SF_SOA_HR_02: SFSOAHR01,
+  SF_SOA_HR_03: SFSOAHR01,
+  SF_SOA_HR_04: SFSOAHR01,
+  SF_SOA_HR_05: SFSOAHR01,
   "SOA-IT-001": SOAIT001,
   Trans: Transcript
 };
