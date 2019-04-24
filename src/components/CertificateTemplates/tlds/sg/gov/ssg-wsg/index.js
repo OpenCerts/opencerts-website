3ff40ff6b8e2fe203b831f11ec93d87940384729
ssgwsg-acn <49136659+ssgwsg-acn@users.noreply.github.com>
2019-04-24 09:49:44 +0530

    Added SOA Templates (SFSOAIT001,SFSOAMF01,SOAMF01) for SSG (#193)
    * Added SOA Templates (SFSOAIT001,SFSOAMF01,SOAMF01)

--- src/components/CertificateTemplates/tlds/sg/gov/ssg-wsg/index.js
+++ src/components/CertificateTemplates/tlds/sg/gov/ssg-wsg/index.js
@@ -49,6 +49,17 @@ const SOAIT001 = dynamic(() =>
   import("./SOA-IT-001" /* webpackChunkName: "SSGTemplates" */)
 );
 
+const SFSOAIT001 = dynamic(() =>
+  import("./SF_SOA_IT_001" /* webpackChunkName: "SSGTemplates" */)
+);
+
+const SOAMF01 = dynamic(() =>
+  import("./SOA-MF-01" /* webpackChunkName: "SSGTemplates" */)
+);
+
+const SFSOAMF01 = dynamic(() =>
+  import("./SF_SOA_MF_01" /* webpackChunkName: "SSGTemplates" */)
+);
 const Transcript = dynamic(() =>
   import("./Trans" /* webpackChunkName: "SSGTemplates" */)
 );
@@ -71,6 +82,11 @@ const templates = {
   "SOA-002": SOA002,
   "SOA-003": SOA003,
   "SOA-IT-001": SOAIT001,
+  SF_SOA_IT_001: SFSOAIT001,
+  "SOA-MF-01": SOAMF01,
+  SOA_MF_02: SOAMF01,
+  SF_SOA_MF_01: SFSOAMF01,
+  SF_SOA_MF_02: SFSOAMF01,
   Trans: Transcript
 };
 
