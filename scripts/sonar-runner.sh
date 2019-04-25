54099eddcba7fa169b7c51a314203100b0dce6d3
Raymond Yeh <yehjxraymond@gmail.com>
2019-04-25 15:03:00 +0800

    Testing manual sonarqube

---
+++ scripts/sonar-runner.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+if ! [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
+    sonar-scanner \
+        -Dsonar.projectKey=OpenCerts_opencerts-website \
+        -Dsonar.organization=opencerts \
+        -Dsonar.sources=. \
+        -Dsonar.host.url=https://sonarcloud.io \
+        -Dsonar.login=${SONAR_LOGIN} \
+        -Dsonar.pullrequest.key=${TRAVIS_PULL_REQUEST} \
+        -Dsonar.pullrequest.branch=${TRAVIS_PULL_REQUEST_BRANCH}
+elif [ "${TRAVIS_PULL_REQUEST}" = "master" ]; then
+    sonar-scanner \
+        -Dsonar.projectKey=OpenCerts_opencerts-website \
+        -Dsonar.organization=opencerts \
+        -Dsonar.sources=. \
+        -Dsonar.host.url=https://sonarcloud.io \
+        -Dsonar.login=${SONAR_LOGIN} \
+fi;
