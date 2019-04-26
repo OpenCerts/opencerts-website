85049caff711f4e631931c79ccfc6032478c10c9
yehjxraymond <yehjxraymond@gmail.com>
2019-04-26 15:26:03 +0800

    Fixed command for master branch (#215)

--- scripts/sonar-runner.sh
+++ scripts/sonar-runner.sh
@@ -11,5 +11,11 @@ if ! [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
         -Dsonar.pullrequest.key=${TRAVIS_PULL_REQUEST} \
         -Dsonar.pullrequest.branch=${TRAVIS_PULL_REQUEST_BRANCH}
 elif [ "${TRAVIS_BRANCH}" = "master" ]; then
-    sonar-scanner
+    sonar-scanner \
+        -Dsonar.scanner.skip=false \
+        -Dsonar.projectKey=OpenCerts_opencerts-website \
+        -Dsonar.organization=opencerts \
+        -Dsonar.sources=. \
+        -Dsonar.host.url=https://sonarcloud.io \
+        -Dsonar.login=${SONAR_LOGIN}
 fi;
