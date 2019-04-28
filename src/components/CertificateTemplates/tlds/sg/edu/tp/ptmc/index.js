029d55e91effd743f78443d6ec67aca5764d1305
Khun Shwe Phyo <khunshwephyo@gmail.com>
2019-04-28 23:38:13 +0800

    Add template for part-time modular certificate and post-diploma certificate

---
+++ src/components/CertificateTemplates/tlds/sg/edu/tp/ptmc/index.js
@@ -0,0 +1,27 @@
+import PropTypes from "prop-types";
+import { MultiCertificateRenderer } from "template-utils/MultiCertificateRenderer";
+
+import TPCert from "./certificate";
+import ApprovedAddresses from "../common";
+
+const templates = [
+  {
+    id: "certificate",
+    label: "Certificate",
+    template: TPCert
+  }
+];
+
+const ptmc = ({ certificate }) => (
+  <MultiCertificateRenderer
+    certificate={certificate}
+    templates={templates}
+    whitelist={ApprovedAddresses}
+  />
+);
+
+ptmc.propTypes = {
+  certificate: PropTypes.object.isRequired
+};
+
+export default ptmc;
---
+++ src/components/CertificateTemplates/tlds/sg/edu/tp/ptpdc/index.js
@@ -0,0 +1,27 @@
+import PropTypes from "prop-types";
+import { MultiCertificateRenderer } from "template-utils/MultiCertificateRenderer";
+
+import TPCert from "../ptmc/certificate";
+import ApprovedAddresses from "../common";
+
+const templates = [
+  {
+    id: "certificate",
+    label: "Certificate",
+    template: TPCert
+  }
+];
+
+const ptpdc = ({ certificate }) => (
+  <MultiCertificateRenderer
+    certificate={certificate}
+    templates={templates}
+    whitelist={ApprovedAddresses}
+  />
+);
+
+ptpdc.propTypes = {
+  certificate: PropTypes.object.isRequired
+};
+
+export default ptpdc;
