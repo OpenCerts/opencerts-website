a7b30d5aa9b9f8c523f015b278487d363a98c144
Khun Shwe Phyo <khunshwephyo@gmail.com>
2019-05-05 23:44:00 +0800

    Update sample.opencert files.
    Change JSON generation logic to exclude properties that are NULL.

--- src/components/CertificateTemplates/tlds/sg/edu/tp/ftdipplus/certificateBody.js
+++ src/components/CertificateTemplates/tlds/sg/edu/tp/ftdipplus/certificateBody.js
@@ -1,31 +1,40 @@
 import PropTypes from "prop-types";
 import CertificateBodyStyles from "../common/certificateBodyStyles";
 
-const CertificateBody = ({ certificate }) => (
-  <div className="container">
-    <CertificateBodyStyles />
+const CertificateBody = ({ certificate }) => {
 
-    <div className="certificate-body">
-      <div className="recipient-paragraph">It is hereby certified that</div>
-      <div className="recipient-name">{certificate.recipient.name}</div>
-      <div className="recipient-paragraph">
-        having satisfied the requirements of the Diploma Plus Programme was
-        awarded the
-      </div>
-      <div className="certificate-name">
-        <span>{certificate.name}</span>
-      </div>
-      <div className="recipient-paragraph">
-        by Temasek Polytechnic (Singapore) on{" "}
-        {new Date(certificate.issuedOn).toLocaleDateString("en-SG", {
-          day: "numeric",
-          month: "long",
-          year: "numeric"
-        })}
+  const firstLine = "Certificate in";
+  const secondLine = certificate.name.replace(/certificate in/i, "").trim();
+
+  return (
+    <div className="container">
+      <CertificateBodyStyles />
+
+      <div className="certificate-body">
+        <div className="recipient-paragraph">It is hereby certified that</div>
+        <div className="recipient-name">{certificate.recipient.name}</div>
+        <div className="recipient-paragraph">
+          having satisfied the requirements of the Diploma Plus Programme was
+          awarded the
+        </div>
+        <div className="certificate-name">
+          {firstLine}
+          <br />
+          {secondLine}
+        </div>
+        <div className="recipient-paragraph">
+          by Temasek Polytechnic (Singapore) on{" "}
+          {new Date(certificate.issuedOn).toLocaleDateString("en-SG", {
+            day: "numeric",
+            month: "long",
+            year: "numeric"
+          })}
+        </div>
       </div>
     </div>
-  </div>
-);
+  );
+
+};
 
 CertificateBody.propTypes = {
   certificate: PropTypes.object.isRequired
