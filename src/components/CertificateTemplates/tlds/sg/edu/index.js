a7c6e0e269f7000c7912c4a96ecbdde6cde03f6b
laykoonjune <june_lk_lim@nyp.edu.sg>
2019-05-09 16:55:22 +0800

    fix no raster image rule

--- src/components/CertificateTemplates/tlds/sg/edu/index.js
+++ src/components/CertificateTemplates/tlds/sg/edu/index.js
@@ -3,10 +3,6 @@ import { addDirToTemplatePath } from "template-utils/addDirToTemplatePath";
 import singaporetech from "./singaporetech";
 import np from "./np";
 import nyp from "./nyp";
-<<<<<<< HEAD
-
-export default addDirToTemplatePath("edu", { ...singaporetech, ...np, ...nyp });
-=======
 import tp from "./tp";
 
 export default addDirToTemplatePath("edu", {
@@ -15,4 +11,3 @@ export default addDirToTemplatePath("edu", {
   ...nyp,
   ...tp
 });
->>>>>>> ecea7975d867786bb02ec96b9bef1bf8ce275aea
