47925ac208c56583ef7b8fa9e69f28fd4895d26f
erwinsg <43283667+erwinsg@users.noreply.github.com>
2019-05-09 15:33:56 +0800

    SEAB GCEO and GCEN (#250)

--- src/components/CertificateTemplates/tlds/sg/gov/seab/SOR_GCEN_NT_2008/sor.js
+++ src/components/CertificateTemplates/tlds/sg/gov/seab/SOR_GCEN_NT_2008/sor.js
@@ -1,7 +1,7 @@
 import { TEMPLATE_ESOR } from "../common/template";
 
 const Template = certificate => (
-  <span>{TEMPLATE_ESOR(certificate, "GCEO", "O")}</span>
+  <span>{TEMPLATE_ESOR(certificate, "GCEN", "NT")}</span>
 );
 
 export default Template;
