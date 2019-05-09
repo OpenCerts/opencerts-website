04da2bfee94ab8ea487831fcf8726cee090e48a7
Pat-Goh <48708518+Pat-Goh@users.noreply.github.com>
2019-05-09 19:13:55 +0800

    Adding new custom-templates, and registry identity for ITE. (#237)
    * Adding new custom-templates, and registry identity for ITE.

---
+++ src/components/CertificateTemplates/tlds/sg/edu/ite/common/index.js
@@ -0,0 +1,7 @@
+export * from "./images";
+export { default as Certificate } from "./certificate";
+
+export const approvedAddresses = [
+  "0xE5c693015460868F817799CAFb363EEDb4F9E446", // ROPSTEN: ITE
+  "0x908595e0B09Fb2d5950eCD196695490E211A6d4d" // ITE
+];
