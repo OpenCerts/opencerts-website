0b89d4c8fad282d0095bc3cdfbe196b4e3cdf902
NCS Blockchain <ncs.com.sg>
2019-05-10 11:53:33 +0530

    Modified index.js file

--- src/components/CertificateTemplates/tlds/sg/com/ncs/index.js
+++ src/components/CertificateTemplates/tlds/sg/com/ncs/index.js
@@ -1,13 +1,16 @@
 import dynamic from "template-utils/dynamic";
+import { addDirToTemplatePath } from "template-utils/addDirToTemplatePath";
 
-const NCS01 = dynamic(
+const NCS01 = dynamic(() =>
   import("./2019-04-NCS-01" /* webpackChunkName: "NCSTemplates" */)
 );
-const NCS02 = dynamic(
+const NCS02 = dynamic(() =>
   import("./2019-04-NCS-02" /* webpackChunkName: "NCSTemplates" */)
 );
 
-export default {
+const templates = {
   "2019-04-NCS-01": NCS01,
   "2019-04-NCS-02": NCS02
 };
+
+export default addDirToTemplatePath("ncs", templates);
