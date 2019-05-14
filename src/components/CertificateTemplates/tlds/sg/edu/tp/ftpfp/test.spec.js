dd5e64b15584976dafdb76b304af3fa50dab21a7
Khun Shwe Phyo <khunshwephyo@gmail.com>
2019-05-14 11:30:14 +0800

    updated integration tests for full-time certs

--- src/components/CertificateTemplates/tlds/sg/edu/tp/ftpfp/test.spec.js
+++ src/components/CertificateTemplates/tlds/sg/edu/tp/ftpfp/test.spec.js
@@ -23,7 +23,13 @@ test("Polytechnic Foundation Programme is rendered correctly.", async t => {
   await validateTextContent(t, RenderedCertificate, [
     "DUMMY STUDENT NAME",
     "Polytechnic",
-    "Foundation Programme"
+    "Foundation Programme",
+    "Academic Year",
+    "at Temasek Polytechnic (Singapore)",
+    "Director",
+    "School of Humanities",
+    "& Social Sciences",
+    "Registrar"
   ]);
 
   const transcriptTab = TemplateTabList.find(":nth-child(2)");
@@ -32,11 +38,31 @@ test("Polytechnic Foundation Programme is rendered correctly.", async t => {
   // transcript tab content
   await validateTextContent(t, RenderedCertificate, [
     "DUMMY STUDENT NAME",
+    "999",
+    "Dummy street name 1",
+    "Dummy street name 2",
+    "Dummy street name 3",
+    "01",
+    "Singapore",
+    "999999",
     "S0000000A",
     "1234567A",
+    "Dummy course type",
+    "credit units were granted based on the following exemptions",
+    "S001",
+    "Dummy01 exempted subject name",
+    "5",
+    "Exempted",
+    "AY9998/9999",
+    "Dummy0 Semester",
     "S001",
     "Dummy01 subject name",
+    "Dummy23 subject name *",
+    "Dummy24 subject name #",
     "COMPLETED POLYTECHNIC FOUNDATION PROGRAMME",
-    "Grading System"
+    "# The candidate was granted partial exemption from the assessment objectives of this subject.",
+    "* The candidate was granted full exemption from the assessment objectives of this subject.",
+    "Grading System",
+    "for REGISTRAR"
   ]);
 });
