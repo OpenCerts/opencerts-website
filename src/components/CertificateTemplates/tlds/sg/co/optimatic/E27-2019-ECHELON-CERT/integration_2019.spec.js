7a1ad035a6800f5d0de525137f6ede6d62fb73e7
Wong Wai Chung <wai1chung@gmail.com>
2019-05-14 21:02:22 +0800

    chore: updated integration tests to deal issuer non-identification

--- src/components/CertificateTemplates/tlds/sg/co/optimatic/E27-2019-ECHELON-CERT/integration_2019.spec.js
+++ src/components/CertificateTemplates/tlds/sg/co/optimatic/E27-2019-ECHELON-CERT/integration_2019.spec.js
@@ -3,13 +3,17 @@ import { Selector } from "testcafe";
 fixture("Optimatic Pte Ltd").page`http://localhost:3000`;
 
 const Certificate = "./E27-TEST-2019-ECHELON-CERT.opencert";
-
-// Only Certficate, No Transcript
+const ButtonViewCertificateAnyway = Selector("#certificate-view-anyway");
+const CertificateVerifyBlock = Selector("#certificate-verify-block");
 const RenderedCertificate = Selector("#rendered-certificate");
 
 test("E27-TEST-2019-ECHELON-CERT certificate is rendered correctly", async t => {
   // Uploads certificate via dropzone
   await t.setFilesToUpload("input[type=file]", [Certificate]);
+  await t.click(ButtonViewCertificateAnyway);
+  await t
+    .expect(CertificateVerifyBlock.textContent)
+    .contains("Unknown Institution");
 
   await t.expect(RenderedCertificate.visible).ok();
   await t.expect(RenderedCertificate.textContent).contains("Mother of Dragons");
