4ddb74639fb77d63e7f88b630934e4be4db46bc4
Raymond Yeh <yehjxraymond@gmail.com>
2019-05-23 16:21:08 +0800

    Added sample iframe.html

--- src/components/FramelessViewer/FramelessViewerPageContainer.js
+++ src/components/FramelessViewer/FramelessViewerPageContainer.js
@@ -1,4 +1,4 @@
-import React, { Component, useEffect } from "react";
+import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import {
@@ -11,59 +11,60 @@ import styles from "../certificateViewer.scss";
 import {
   updateCertificate,
   getCertificate,
-  getTemplates,
-  selectTemplateTab
+  getTemplates as getTemplatesAction,
+  getActiveTemplateTab,
+  selectTemplateTab as selectTemplateTabAction
 } from "../../reducers/certificate";
 import FramelessCertificateViewer from "./FramelessCertificateViewer";
 import { getLogger } from "../../utils/logger";
 
 const { trace } = getLogger("components:FramelessViewerPageContainer");
 
-const inIframe = window.location !== window.parent.location;
-
+const inIframe = () => window.location !== window.parent.location;
+const flatten = o => JSON.parse(JSON.stringify(o));
 class FramelessViewerContainer extends Component {
   constructor(props) {
     super(props);
 
     this.handleCertificateChange = this.handleCertificateChange.bind(this);
     this.handleTextFieldChange = this.handleTextFieldChange.bind(this);
+    this.state = { parentFrameConnection: null };
   }
 
   componentDidUpdate() {
-    inIframe &&
-      connectToParent().promise.then(parent =>
-        parent.updateHeight(document.documentElement.scrollHeight)
-      );
+    if (inIframe()) {
+      this.state.parentFrameConnection.promise.then(parent => {
+        if (parent.updateHeight)
+          parent.updateHeight(document.documentElement.scrollHeight);
+        if (parent.updateTemplates)
+          parent.updateTemplates(flatten(this.props.templates));
+      });
+    }
   }
 
   componentDidMount() {
-    const getTempates = () => this.props.templates.bind(this);
+    const { selectTemplateTab } = this.props;
+    const getTemplates = (() => flatten(this.props.templates)).bind(this);
     const renderCertificate = this.handleCertificateChange;
-    const selectTemplateTab = this.props.selectTemplateTab;
+    const frameHeight = document.documentElement.scrollHeight;
 
     window.opencerts = {
-      getTempates,
+      getTemplates,
       renderCertificate,
       selectTemplateTab
     };
 
-    inIframe &&
-      connectToParent({
+    if (inIframe()) {
+      const parentFrameConnection = connectToParent({
         methods: {
-          renderCertificate(certificate) {
-            renderCertificate(certificate);
-          },
-          selectTemplateTab(index) {
-            selectTemplateTab(index);
-          },
-          getTemplates() {
-            console.log(this.props.templates);
-          },
-          height() {
-            return document.documentElement.scrollHeight;
-          }
+          renderCertificate,
+          selectTemplateTab,
+          getTemplates,
+          frameHeight
         }
       });
+      this.setState({ parentFrameConnection });
+    }
   }
 
   handleTextFieldChange(e) {
@@ -106,12 +107,13 @@ class FramelessViewerContainer extends Component {
 
 const mapStateToProps = store => ({
   document: getCertificate(store),
-  templates: getTemplates(store)
+  templates: getTemplatesAction(store),
+  activeTab: getActiveTemplateTab(store) // required to trigger componentDidUpdate when tab changes
 });
 
 const mapDispatchToProps = dispatch => ({
   updateCertificate: payload => dispatch(updateCertificate(payload)),
-  selectTemplateTab: tabIndex => dispatch(selectTemplateTab(tabIndex))
+  selectTemplateTab: tabIndex => dispatch(selectTemplateTabAction(tabIndex))
 });
 
 export default connect(
---
+++ src/components/FramelessViewer/integration-frameless-iframe.spec.js
@@ -0,0 +1,40 @@
+import { Selector } from "testcafe";
+
+fixture("Frameless Viewer").page`./Iframe.html`;
+
+const RenderButton = Selector("#render-certificate");
+const IFrame = Selector("#frameless-iframe");
+const RenderedCertificate = Selector("#rendered-certificate");
+const TranscriptButton = Selector("#selector-transcript");
+
+const validateTextContent = async (t, component, texts) =>
+  texts.reduce(
+    async (_prev, curr) => t.expect(component.textContent).contains(curr),
+    Promise.resolve()
+  );
+
+test("Default certificate is rendered correctly", async t => {
+  // Render loaded certificate defined in the html
+  await t.click(RenderButton);
+
+  // Switch context to within iframe
+  await t.switchToIframe(IFrame);
+
+  // Validate content of first tab
+  await validateTextContent(t, RenderedCertificate, [
+    "Principal",
+    "Council Chairman",
+    "A3DA180001"
+  ]);
+
+  // Click on transcript button
+  await t.switchToMainWindow();
+  await t.click(TranscriptButton);
+  await t.switchToIframe(IFrame);
+
+  // Validate content of second tab
+  await validateTextContent(t, RenderedCertificate, [
+    "TRANSCRIPT OF ACADEMIC RECORD",
+    "DIRECTOR, ACADEMIC AFFAIRS"
+  ]);
+});
