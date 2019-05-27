305f370b0b1c9ae11325a6b2bbe5c529df42b31d
Khun Shwe Phyo <khunshwephyo@gmail.com>
2019-05-27 18:36:29 +0800

    Refactor certificate name splitting logic

--- src/components/CertificateTemplates/tlds/sg/edu/tp/common/certificateNameSection.js
+++ src/components/CertificateTemplates/tlds/sg/edu/tp/common/certificateNameSection.js
@@ -1,19 +1,20 @@
 import PropTypes from "prop-types";
 
 const CertificateNameSection = ({ certificate }) => {
+  const splitTwoLines = value => {
+    const index = value.search(/ in /i);
 
-  const splitTwoLines = (value) => {
-    const index = value.search(/ in /i); 
-
-    if(index > 0) {
-
+    if (index > 0) {
       const firstLineEnd = index + 3;
       const firstLine = certificate.name.substring(0, firstLineEnd);
-      
+
       const secondLineStart = index + 4;
       const secondLindEnd = certificate.name.length;
-      const secondLine = certificate.name.substring(secondLineStart, secondLindEnd);
-      
+      const secondLine = certificate.name.substring(
+        secondLineStart,
+        secondLindEnd
+      );
+
       return (
         <span>
           {firstLine}
@@ -21,34 +22,24 @@ const CertificateNameSection = ({ certificate }) => {
           {secondLine}
         </span>
       );
-    } else {
-      return (
-        <span>
-        {certificate.name}
-        </span>
-      );
     }
+    return <span>{certificate.name}</span>;
   };
 
   return (
     <div className="certificate-name">
-      
-      {splitTwoLines(certificate.name) }
+      {splitTwoLines(certificate.name)}
 
       {certificate.additionalData.isMerit && (
         <span>
-        <br />
-        <small>WITH MERIT</small>
+          <br />
+          <small>WITH MERIT</small>
         </span>
       )}
 
-      {certificate.additionalData.isExempted && (
-        <span>*</span>
-      )}
-
+      {certificate.additionalData.isExempted && <span>*</span>}
     </div>
   );
-  
 };
 
 CertificateNameSection.propTypes = {
