1750884acfb6f125675d2111a4229795b06fa9e9
Khun Shwe Phyo <khunshwephyo@gmail.com>
2019-05-31 12:50:50 +0800

    part-time - add modular course certificates

--- src/components/CertificateTemplates/tlds/sg/edu/tp/index.js
+++ src/components/CertificateTemplates/tlds/sg/edu/tp/index.js
@@ -33,6 +33,14 @@ const ptpdc = dynamic(() =>
   import("./ptpdc" /* webpackChunkName: "TemasekPolytechnicTemplates" */)
 );
 
+const ptmcModularCourse = dynamic(() =>
+  import("./ptmc_mod" /* webpackChunkName: "TemasekPolytechnicTemplates" */)
+);
+
+const ptpdcModularCourse = dynamic(() =>
+  import("./ptpdc_mod" /* webpackChunkName: "TemasekPolytechnicTemplates" */)
+);
+
 const ptdipdpss = dynamic(() =>
   import("./ptdipdpss" /* webpackChunkName: "TemasekPolytechnicTemplates" */)
 );
@@ -298,6 +306,49 @@ const templates = {
   "ptmc-2027": ptmc,
   "ptmc-2028": ptmc,
   "ptmc-2029": ptmc,
+
+  "ptmc-mod-2010": ptmcModularCourse,
+  "ptmc-mod-2011": ptmcModularCourse,
+  "ptmc-mod-2012": ptmcModularCourse,
+  "ptmc-mod-2013": ptmcModularCourse,
+  "ptmc-mod-2014": ptmcModularCourse,
+  "ptmc-mod-2015": ptmcModularCourse,
+  "ptmc-mod-2016": ptmcModularCourse,
+  "ptmc-mod-2017": ptmcModularCourse,
+  "ptmc-mod-2018": ptmcModularCourse,
+  "ptmc-mod-2019": ptmcModularCourse,
+  "ptmc-mod-2020": ptmcModularCourse,
+  "ptmc-mod-2021": ptmcModularCourse,
+  "ptmc-mod-2022": ptmcModularCourse,
+  "ptmc-mod-2023": ptmcModularCourse,
+  "ptmc-mod-2024": ptmcModularCourse,
+  "ptmc-mod-2025": ptmcModularCourse,
+  "ptmc-mod-2026": ptmcModularCourse,
+  "ptmc-mod-2027": ptmcModularCourse,
+  "ptmc-mod-2028": ptmcModularCourse,
+  "ptmc-mod-2029": ptmcModularCourse,
+
+  "ptpdc-mod-2010": ptpdcModularCourse,
+  "ptpdc-mod-2011": ptpdcModularCourse,
+  "ptpdc-mod-2012": ptpdcModularCourse,
+  "ptpdc-mod-2013": ptpdcModularCourse,
+  "ptpdc-mod-2014": ptpdcModularCourse,
+  "ptpdc-mod-2015": ptpdcModularCourse,
+  "ptpdc-mod-2016": ptpdcModularCourse,
+  "ptpdc-mod-2017": ptpdcModularCourse,
+  "ptpdc-mod-2018": ptpdcModularCourse,
+  "ptpdc-mod-2019": ptpdcModularCourse,
+  "ptpdc-mod-2020": ptpdcModularCourse,
+  "ptpdc-mod-2021": ptpdcModularCourse,
+  "ptpdc-mod-2022": ptpdcModularCourse,
+  "ptpdc-mod-2023": ptpdcModularCourse,
+  "ptpdc-mod-2024": ptpdcModularCourse,
+  "ptpdc-mod-2025": ptpdcModularCourse,
+  "ptpdc-mod-2026": ptpdcModularCourse,
+  "ptpdc-mod-2027": ptpdcModularCourse,
+  "ptpdc-mod-2028": ptpdcModularCourse,
+  "ptpdc-mod-2029": ptpdcModularCourse,
+
 
   "ptpdc-2010": ptpdc,
   "ptpdc-2011": ptpdc,
