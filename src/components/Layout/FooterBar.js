54dc070e2007810288a3ffb14cbe85b2a77aaede
samuellye98 <sl2982@cornell.edu>
2019-06-13 16:18:45 +0800

    fix footer

--- src/components/Layout/FooterBar.js
+++ src/components/Layout/FooterBar.js
@@ -1,29 +1,25 @@
 import css from "./footer.scss";
 
 const NavigationBar = () => (
-  // <div
-  //   className="bg-brand-dark"
-  //   style={{ paddingTop: "20px", paddingBottom: "20px" }}
-  //   id={css["footer-print"]}
-  // >
-  <div className={css.footer}>
-    <div>
-      <a href="/collaborate">Collaborate</a>
-    </div>
-    <div>
-      <a href="/faq">FAQ</a>
-    </div>
-    <div>
-      <a href="/registry">Registry</a>
-    </div>
-    <div>
-      <a href="/privacy">Privacy Policy</a>
-    </div>
-    <div>
-      <a href="https://github.com/OpenCerts">Github</a>
+  <div className="bg-brand-dark" id={css["footer-print"]}>
+    <div className={css.footer}>
+      <div>
+        <a href="/collaborate">Collaborate</a>
+      </div>
+      <div>
+        <a href="/faq">FAQ</a>
+      </div>
+      <div>
+        <a href="/registry">Registry</a>
+      </div>
+      <div>
+        <a href="/privacy">Privacy Policy</a>
+      </div>
+      <div>
+        <a href="https://github.com/OpenCerts">Github</a>
+      </div>
     </div>
   </div>
-  // </div>
 );
 
 export default NavigationBar;
