862f42d737e620eba6b03600c16aa54ac7ea1a8d
Raymond Yeh <yehjxraymond@gmail.com>
2019-07-09 14:23:25 +0800

    Changed to diagonals

--- src/components/PrintWatermark.js
+++ src/components/PrintWatermark.js
@@ -1,18 +1,23 @@
 const STATEMENT =
-  "Printed OpenCerts certificates cannot be verified. Please ask the certificate holder for their .opencert file. Visit opencerts.io for more information.";
+  "Printed OpenCerts cannot be verified. Please ask the certificate holder for their .opencert file. Visit opencerts.io for more information.";
 const PrintWatermark = () => (
   <div
     className="print-only"
     style={{
       position: "fixed",
-      top: 800,
-      left: 0,
+      top: "50%",
+      left: "50%",
       width: "100%",
       height: "100%",
-      opacity: 0.6,
-      fontSize: "2em",
+      marginRight: "-50%",
+      marginTop: "-50%",
+      opacity: 0.2,
+      fontSize: "4em",
+      fontWeight: "900",
       zIndex: 1000,
-      overflow: "hidden"
+      overflow: "hidden",
+      transform: "rotate(300deg)",
+      textAlign: "center"
     }}
   >
     {STATEMENT}
