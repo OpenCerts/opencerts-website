43c070b12762fe71305a72454c80aaa811ed3ce6
samuellye98 <sl2982@cornell.edu>
2019-07-23 13:56:37 +0800

    Added watermark

--- src/components/PrintWatermark.js
+++ src/components/PrintWatermark.js
@@ -1,26 +1,14 @@
-const STATEMENT =
-  "Printed from the digital version of certificate. For verification, please visit OpenCerts.io (http://opencerts.io/) with the .opencert file.";
 const PrintWatermark = () => (
-  <div
-    className="print-only"
-    style={{
-      position: "fixed",
-      top: "50%",
-      left: "50%",
-      width: "100%",
-      height: "100%",
-      marginRight: "-50%",
-      marginTop: "-50%",
-      opacity: 0.15,
-      fontSize: "4em",
-      fontWeight: "900",
-      zIndex: 1000,
-      overflow: "hidden",
-      transform: "rotate(300deg)",
-      textAlign: "center"
-    }}
-  >
-    {STATEMENT}
+  <div className="print-only">
+    <img
+      style={{
+        position: "absolute",
+        opacity: 0.5,
+        width: "100%",
+        height: "100%"
+      }}
+      src="/static/images/watermark.svg"
+    />
   </div>
 );
 
