5c451b6a40a8c2c05f2f4abb873a3714e7d00710
Jason Kam <thefiend@hotmail.sg>
2019-08-15 14:22:27 +0800

    Added postbuild command

--- post-export.js
+++ post-export.js
@@ -2,4 +2,8 @@
 const { generateSitemap } = require("./generate-sitemap");
 // Generate the sitemap and define the target folder
 // Don't forget to set the PUBLIC_DOMAIN secret in Netlify! 🕵️‍♀️
-generateSitemap("https://opencerts.io", "./out/static/");
+const URL =
+  process.env.NET === "mainnet"
+    ? "https://opencerts.io"
+    : "https://dev.opencerts.io";
+generateSitemap(URL, "./out/static/");
