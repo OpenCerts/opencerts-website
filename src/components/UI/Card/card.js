6dd2daa4ca2b80669ca34afed9ee2727a766e939
Nebulis <nebounet@gmail.com>
2020-04-30 16:29:53 +0800

    feat: group partners

--- src/components/UI/Card/card.js
+++ src/components/UI/Card/card.js
@@ -3,71 +3,73 @@ import css from "./card.scss";
 
 const Card = props => (
   <div
-    id={props.info.id}
+    id={props.info[0].id}
     className={`col-lg-4 col-md-6 col-sm-12 ${css["mb-3"]} ${css["m-pd-0"]}`}
     style={{ paddingBottom: "36px", wordWrap: "break-word" }}
   >
-    <a
-      href={props.info.website}
-      target="_blank"
-      rel="noopener noreferrer nofollow"
-      style={{ color: "#000", textDecoration: "none" }}
+    <span
+      onClick={() => window.open(props.info[0].website)}
+      style={{ color: "#000", textDecoration: "none", cursor: "pointer" }}
     >
-      <div className={css["partner-block"]}>
-        <img
-          className={`${css.logo}`}
-          src={props.info.logo}
-          id={css[props.info.key]}
-        />
+      <div className={css["partner-block"]} key={props.info[0].id}>
         <h4 className={css["partner-name"]} style={{ fontWeight: "bold" }}>
-          {props.info.name}
+          {props.info[0].name}
         </h4>
-        {props.info.address ? (
-          <div>
-            Certificate Store:{" "}
-            <a href={`https://etherscan.io/address/${props.info.address}`}>
-              {props.info.address}
-            </a>
+        <img className={`${css.logo}`} src={props.info[0].logo} />
+        {props.info.map((info, index) => (
+          <div key={info.id} className={css["partner-info"]}>
+            {info.address ? (
+              <div>
+                {index > 0 && `${info.name} `}
+                Certificate Store:{" "}
+                <a
+                  href={`https://etherscan.io/address/${info.address}`}
+                  onClick={event => event.stopPropagation()}
+                >
+                  {info.address}
+                </a>
+              </div>
+            ) : (
+              ""
+            )}
+            {info.address && info.website ? (
+              <div>
+                Website:{" "}
+                <a
+                  href={info.website}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  {info.website}
+                </a>
+              </div>
+            ) : (
+              ""
+            )}
+            {info.email ? (
+              <div>
+                Email: <a href={`mailto:${info.email}`}>{info.email}</a>
+              </div>
+            ) : (
+              ""
+            )}
+            {info.phone ? (
+              <div>
+                Phone: <a href={`tel:${info.phone}`}>{info.phone}</a>
+              </div>
+            ) : (
+              ""
+            )}
+            {info.description ? <div>{info.description}</div> : ""}
           </div>
-        ) : (
-          ""
-        )}
-        {props.info.address && props.info.website ? (
-          <div>
-            Website:{" "}
-            <a
-              href={props.info.website}
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              {props.info.website}
-            </a>
-          </div>
-        ) : (
-          ""
-        )}
-        {props.info.email ? (
-          <div>
-            Email: <a href={`mailto:${props.info.email}`}>{props.info.email}</a>
-          </div>
-        ) : (
-          ""
-        )}
-        {props.info.phone ? (
-          <div>
-            Phone: <a href={`tel:${props.info.phone}`}>{props.info.phone}</a>
-          </div>
-        ) : (
-          ""
-        )}
-        {props.info.description ? <div>{props.info.description}</div> : ""}
+        ))}
       </div>
-    </a>
+    </span>
   </div>
 );
 
 export default Card;
 
 Card.propTypes = {
-  info: PropTypes.object
+  info: PropTypes.array
 };
