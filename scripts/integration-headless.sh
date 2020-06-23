14bea8d479367c9a71267785716912643e2e00c9
Goi Jia Jian <jiajiannn@gmail.com>
2020-06-23 13:23:11 +0800

    fix: encode opencert data on download (#513)
    * fix: encode opencert data on download (#510)
    
    * fix: encode opencert data
    
    - if an opencert file contains special characters like #, the download feature will only download the contents preceding the #
    
    * feat: e2e test for cert download
    
    * feat: e2e test for cert download
    
    - removed #download-link id from download link, no longer needed
    - added 2 e2e tests to ensure documents with special characters can be downloaded
    
    * fix: added package and package lock
    
    - which was left out.....
    
    * feat: changed download wait function
    
    - from a hardcoded 5s to every 100ms, timeout after 10s
    
    * fix: update package lock
    
    * fix: default download path in ci docker
    
    - somehow circleci's docker container cannot run xdg-user-dir
    
    * fix: removed test
    
    - cos idk the default download folder.. and xdg-user-dir doesn't work in the circleci docker container
    
    * fix: add test back
    
    * chore: add ssh for circleci debug
    
    * chore: remove ssh key
    
    * fix: remove downloads-folder package
    
    - reason: the plugin throws an exception when xdg-user-dir is not found, which inadvertedly stops our circleci integration tests (but when ssh-ing into the circleci machine, it actually continues to run...)
    - so i made the decision to just not use it. instead, hardcode with ~/Downloads as that seems to be the circleci's default downloads folder
    
    * fix: *nix folder path to forward slash
    
    * fix: change to process.env.HOME
    
    * fix: path
    
    * fix: include downloads-folder in test setup script
    
    * fix: lint
    
    * fix: path
    
    * fix: upgrade downloads-folder to v2
    
    * fix: shifted test cleanup to afterEach hook
    
    - also removed mkdir ~/Downloads in integration-headless as it defeats the purpose of xdg-user-dir
    
    * fix: lint
    
    * fix: comments

--- scripts/integration-headless.sh
+++ scripts/integration-headless.sh
@@ -1,2 +1,3 @@
+sudo apt-get install -y xdg-user-dirs && \
 npm run build:static && \
 npm run integration:headless -- --app "npm run serve-static"
---
+++ src/components/tests/download-certificate.spec.js
@@ -0,0 +1,62 @@
+import { existsSync, readFileSync, unlinkSync } from "fs";
+import downloadsFolder from "downloads-folder";
+import { Selector } from "testcafe";
+import TestDocument2 from "./fixture/sample-dns-verified-special-characters.json";
+import TestDocument1 from "./fixture/sample-dns-verified.json";
+
+fixture("Download Certificate").page`http://localhost:3000`.afterEach(async (t) => {
+  // Clean up files after each test
+  const filePath = t.ctx.filePath;
+  await unlinkSync(filePath);
+});
+
+const Document1 = "./fixture/sample-dns-verified.json";
+const Document2 = "./fixture/sample-dns-verified-special-characters.json";
+const StatusButton = Selector("#certificate-status");
+const DownloadLink = Selector("a").withAttribute("download");
+const DownloadButton = Selector("#btn-download");
+
+const validateTextContent = async (t, component, texts) =>
+  texts.reduce(async (_prev, curr) => t.expect(component.textContent).contains(curr), Promise.resolve());
+
+// From https://stackoverflow.com/a/57624660/950462
+const waitForFileDownload = async (t, filePath) => {
+  // Timeout after 10 seconds
+  for (let i = 0; i < 100; i++) {
+    if (existsSync(filePath)) return true;
+    await t.wait(100);
+  }
+  return existsSync(filePath);
+};
+
+test("Sample document is downloaded correctly", async (t) => {
+  await t.setFilesToUpload("input[type=file]", [Document1]);
+
+  await validateTextContent(t, StatusButton, ["Certificate issued by EXAMPLE.OPENATTESTATION.COM"]);
+
+  // Simulate an OpenCert file download
+  const fileName = await DownloadLink.getAttribute("download");
+  await t.click(DownloadButton);
+  const filePath = `${downloadsFolder()}/${fileName}`;
+  t.ctx.filePath = filePath; // For use in cleanup
+  await t.expect(await waitForFileDownload(t, filePath)).eql(true);
+
+  // We expect the contents of the input to match the downloaded file
+  await t.expect(JSON.parse(readFileSync(filePath, "utf8"))).eql(TestDocument1);
+});
+
+test("Sample document with special characters is downloaded correctly", async (t) => {
+  await t.setFilesToUpload("input[type=file]", [Document2]);
+
+  await validateTextContent(t, StatusButton, ["Certificate issued by EXAMPLE.OPENATTESTATION.COM"]);
+
+  // Simulate an OpenCert file download
+  const fileName = await DownloadLink.getAttribute("download");
+  await t.click(DownloadButton);
+  const filePath = `${downloadsFolder()}/${fileName}`;
+  t.ctx.filePath = filePath; // For use in cleanup
+  await t.expect(await waitForFileDownload(t, filePath)).eql(true);
+
+  // We expect the contents of the input to match the downloaded file
+  await t.expect(JSON.parse(readFileSync(filePath, "utf8"))).eql(TestDocument2);
+});
