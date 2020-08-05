13ac063b5c4eb44ebe2128f2494afbe45ab4dc22
Goi Jia Jian <jiajiannn@gmail.com>
2020-08-05 16:49:10 +0800

    fix: uncomment expected test

--- src/components/tests/download-certificate.spec.js
+++ src/components/tests/download-certificate.spec.js
@@ -12,12 +12,12 @@ fixture("Download Certificate").page`http://localhost:3000`.afterEach(async (t) 
 
 const Document1 = "./fixture/sample-dns-verified.json";
 const Document2 = "./fixture/sample-dns-verified-special-characters.json";
-// const StatusButton = Selector("#certificate-status");
+const StatusButton = Selector("#certificate-status");
 const DownloadLink = Selector("a").withAttribute("download");
 const DownloadButton = Selector("#btn-download");
 
-// const validateTextContent = async (t, component, texts) =>
-//   texts.reduce(async (_prev, curr) => t.expect(component.textContent).contains(curr), Promise.resolve());
+const validateTextContent = async (t, component, texts) =>
+  texts.reduce(async (_prev, curr) => t.expect(component.textContent).contains(curr), Promise.resolve());
 
 // From https://stackoverflow.com/a/57624660/950462
 const waitForFileDownload = async (t, filePath) => {
@@ -32,7 +32,7 @@ const waitForFileDownload = async (t, filePath) => {
 test("Sample document is downloaded correctly", async (t) => {
   await t.setFilesToUpload("input[type=file]", [Document1]);
 
-  // await validateTextContent(t, StatusButton, ["Certificate issued by EXAMPLE.OPENATTESTATION.COM"]);
+  await validateTextContent(t, StatusButton, ["Certificate issued by EXAMPLE.OPENATTESTATION.COM"]);
 
   // Simulate an OpenCert file download
   const fileName = await DownloadLink.getAttribute("download");
@@ -48,7 +48,7 @@ test("Sample document is downloaded correctly", async (t) => {
 test("Sample document with special characters is downloaded correctly", async (t) => {
   await t.setFilesToUpload("input[type=file]", [Document2]);
 
-  // await validateTextContent(t, StatusButton, ["Certificate issued by EXAMPLE.OPENATTESTATION.COM"]);
+  await validateTextContent(t, StatusButton, ["Certificate issued by EXAMPLE.OPENATTESTATION.COM"]);
 
   // Simulate an OpenCert file download
   const fileName = await DownloadLink.getAttribute("download");
