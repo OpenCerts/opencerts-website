e99abd09f3dbf505b0152d12e20045d2177091a8
Goi Jia Jian <jiajiannn@gmail.com>
2020-08-05 19:00:46 +0800

    chore: check os for integration test (#539)
    Reason: There's no need to install xdg-user-dir as it's only for *nix-based OS, hence I added the OS check.
    
    Recap: The apt-get was previously added to support the downloads-folder plugin for our integration test.

--- scripts/integration-headless.sh
+++ scripts/integration-headless.sh
@@ -1,3 +1,5 @@
-sudo apt-get install -y xdg-user-dirs && \
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    sudo apt-get install -y xdg-user-dirs
+fi
 npm run build:static && \
 npm run integration:headless -- --app "npm run serve-static"
