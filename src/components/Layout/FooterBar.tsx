2d1d45b4dc8d829ef1fa2bca1848967fe42b188e
Sim Boon Long <simboonlong@gmail.com>
2020-08-28 14:27:32 +0800

    chore: footer styling (#548)

--- src/components/Layout/FooterBar.tsx
+++ src/components/Layout/FooterBar.tsx
@@ -3,43 +3,75 @@ import React from "react";
 import css from "./footer.module.scss";
 
 export const FooterBar: React.FunctionComponent = () => (
-  <div className="bg-brand-dark" id={css["footer-print"]}>
-    <div className={css.footer}>
-      <div>
-        <Link href="/collaborate">
-          <a>Collaborate</a>
-        </Link>
-      </div>
-      <div>
-        <Link href="/faq">
-          <a>FAQ</a>
-        </Link>
-      </div>
-      <div>
-        <Link href="/registry">
-          <a>Registry</a>
-        </Link>
-      </div>
-      <div>
-        <a href="https://go.gov.sg/opencerts-feedback" target="_blank" rel="noopener noreferrer">
-          Feedback
-        </a>
-      </div>
-      <div>
-        <Link href="/privacy">
-          <a>Privacy Policy</a>
-        </Link>
-      </div>
-      <div>
-        <Link href="/terms">
-          <a>Terms of Use</a>
-        </Link>
-      </div>
-      <div>
-        <a href="https://github.com/OpenCerts" target="_blank" rel="noopener noreferrer">
-          GitHub
-        </a>
+  <footer className={css.footer}>
+    <div className={`container ${css.content}`}>
+      <div className="row">
+        <div className="col-12 col-md-4 mb-4 mb-md-0">
+          <Link href="/">
+            <a>
+              <img className={css.logo} src="/static/images/opencertslogo.svg" alt="OpenCerts logo" />
+            </a>
+          </Link>
+        </div>
+        <div className="col-12 col-md col-lg-2 mb-4 mb-md-0">
+          <h6>Partners</h6>
+          <ul className="reset-list">
+            <li>
+              <Link href="/registry">
+                <a>Registry</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/collaborate">
+                <a>Collaborate</a>
+              </Link>
+            </li>
+            <li>
+              <a href="https://go.gov.sg/opencerts-feedback" target="_blank" rel="noopener noreferrer">
+                Feedback
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div className="col-12 col-md col-lg-2 mb-4 mb-md-0">
+          <h6>Support</h6>
+          <ul className="reset-list">
+            <li>
+              <Link href="/faq">
+                <a>FAQ</a>
+              </Link>
+            </li>
+            <li>
+              <a href="https://github.com/OpenCerts" target="_blank" rel="noopener noreferrer">
+                GitHub
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div className="col-12 col-md col-lg-2 mb-4 mb-md-0">
+          <h6>Legal</h6>
+          <ul className="reset-list">
+            <li>
+              <Link href="/privacy">
+                <a>Privacy Policy</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/terms">
+                <a>Terms of Use</a>
+              </Link>
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
-  </div>
+    <div className="container text-center">
+      <div className="row">
+        <div className="col-12">
+          <hr />
+          <p>Copyright © 2020 OpenCerts</p>
+        </div>
+      </div>
+    </div>
+  </footer>
 );
