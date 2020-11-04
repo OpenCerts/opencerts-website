947a3266b1459cbdeffc5f57c4a824bbc08c9a36
Sim Boon Long <simboonlong@gmail.com>
2020-11-04 13:01:39 +0800

    fix: ie flex bugs (#574)

--- src/components/Layout/Body.tsx
+++ src/components/Layout/Body.tsx
@@ -5,7 +5,7 @@ interface WrapperProps {
 }
 
 export const Wrapper: React.FunctionComponent<WrapperProps> = ({ children }) => (
-  <div className="flex flex-col min-h-screen wrapper">{children}</div>
+  <div className="flex flex-col h-screen wrapper">{children}</div>
 );
 
 interface MainProps {
@@ -13,5 +13,8 @@ interface MainProps {
 }
 
 export const Main: React.FunctionComponent<MainProps> = ({ children }) => (
-  <main className="flex-1 main">{children}</main>
+  // https://philipwalton.com/articles/normalizing-cross-browser-flexbox-bugs/
+  <main className="main" style={{ flex: "1 0 auto" }}>
+    {children}
+  </main>
 );
--- src/components/UI/RegistryCard/RegistryCard.tsx
+++ src/components/UI/RegistryCard/RegistryCard.tsx
@@ -39,99 +39,94 @@ export const RegistryCard: React.FunctionComponent<RegistryCardProps> = ({ zInde
 
   return (
     <div className="w-full md:w-1/2 lg:w-1/3 px-4" style={{ zIndex: zIndex }}>
-      <div
-        className="relative flex flex-col bg-navy-200 mb-32"
-        style={{ minHeight: "210px" }}
-        data-testid="registry-card"
-      >
+      <div className="ie-fix-min-height">
         <div
-          className="absolute bg-white flex items-center p-4 left-1/2 border border-gray-300"
-          style={{ marginTop: "-60px", marginLeft: "-90px", width: "180px", height: "120px" }}
+          className="relative flex flex-col bg-navy-200 mb-32"
+          style={{ minHeight: "210px" }}
+          data-testid="registry-card"
         >
-          <img className="max-h-full mx-auto" src={contact[0].logo} alt="" data-testid="institute-logo" />
-        </div>
-        <div className="flex-1 pt-16 px-4">
-          <div className="container-fluid">
-            <div className="row">
-              <div className="col-12">
-                <h4 className="font-montserrat font-bold" data-testid="institute-name">
-                  {contact[0].name}
-                </h4>
-              </div>
+          <div
+            className="absolute bg-white flex items-center p-4 left-1/2 border border-gray-300"
+            style={{ marginTop: "-60px", marginLeft: "-90px", width: "180px", height: "120px" }}
+          >
+            <img
+              className="max-h-full mx-auto ie-fix-img-width"
+              src={contact[0].logo}
+              alt=""
+              data-testid="institute-logo"
+            />
+          </div>
+          <div className="flex-1 pt-16 px-4">
+            <h4 className="font-montserrat font-bold" data-testid="institute-name">
+              {contact[0].name}
+            </h4>
+          </div>
+          <div className="relative">
+            <div
+              className={`ease-colors text-white uppercase font-bold text-center p-2 ${
+                expand ? "bg-orange" : "bg-navy-300 hover:bg-navy"
+              }`}
+              role="button"
+              data-testid="btn-contact"
+              onClick={() => {
+                setExpand(!expand);
+              }}
+            >
+              Contact Info
             </div>
-          </div>
-        </div>
-        <div className="relative">
-          <div
-            className={`ease-colors text-white uppercase font-bold text-center p-2 ${
-              expand ? "bg-orange" : "bg-navy-300 hover:bg-navy"
-            }`}
-            role="button"
-            data-testid="btn-contact"
-            onClick={() => {
-              setExpand(!expand);
-            }}
-          >
-            Contact Info
-          </div>
-          <div
-            className={`absolute w-full bg-white shadow-xl left-0 ${expand ? "block" : "hidden"}`}
-            data-testid="contact-info"
-          >
-            <div className="relative overflow-auto py-3" style={{ maxHeight: "360px" }}>
-              <div className="container-fluid">
-                <div className="row">
-                  <div className="col-12">
-                    {contact.map(
-                      (info) =>
-                        info.address?.includes(search) && (
-                          <div key={info.id} className="p-4" data-testid="info">
-                            {info.name && <h5 className="font-bold font-montserrat mb-4">{info.name}</h5>}
-                            {info.address && info.website && (
-                              <a
-                                className="block text-gray-700 hover:text-gray-700 hover:underline"
-                                href={info.website}
-                                target="_blank"
-                                rel="noopener noreferrer"
-                              >
-                                {info.website}
-                              </a>
-                            )}
-                            {info.email && (
-                              <a
-                                className="block text-gray-700 hover:text-gray-700 hover:underline"
-                                href={`mailto:${info.email}`}
-                              >
-                                {info.email}
-                              </a>
-                            )}
-                            {info.phone && (
-                              <a
-                                className="block text-gray-700 hover:text-gray-700 hover:underline"
-                                href={`tel:${info.phone}`}
-                              >
-                                {info.phone}
-                              </a>
-                            )}
-                            {info.description && <div>{info.description}</div>}
-                            {info.address && (
-                              <div className="mt-4">
-                                <div className="text-uppercase">Certificate Store:</div>
-                                <a
-                                  className="block text-gray-700 hover:text-gray-700 hover:underline break-words"
-                                  href={`https://etherscan.io/address/${info.address}`}
-                                  target="_blank"
-                                  rel="noopener noreferrer"
-                                >
-                                  {info.address}
-                                </a>
-                              </div>
-                            )}
+            <div
+              className={`absolute w-full bg-white shadow-xl left-0 ${expand ? "block" : "hidden"}`}
+              data-testid="contact-info"
+            >
+              <div className="relative overflow-auto py-3" style={{ maxHeight: "360px" }}>
+                {contact.map(
+                  (info) =>
+                    info.address?.includes(search) && (
+                      <div key={info.id} className="p-4" data-testid="info">
+                        {info.name && <h5 className="font-bold font-montserrat mb-4">{info.name}</h5>}
+                        {info.address && info.website && (
+                          <a
+                            className="block text-gray-700 hover:text-gray-700 hover:underline"
+                            href={info.website}
+                            target="_blank"
+                            rel="noopener noreferrer"
+                          >
+                            {info.website}
+                          </a>
+                        )}
+                        {info.email && (
+                          <a
+                            className="block text-gray-700 hover:text-gray-700 hover:underline"
+                            href={`mailto:${info.email}`}
+                          >
+                            {info.email}
+                          </a>
+                        )}
+                        {info.phone && (
+                          <a
+                            className="block text-gray-700 hover:text-gray-700 hover:underline"
+                            href={`tel:${info.phone}`}
+                          >
+                            {info.phone}
+                          </a>
+                        )}
+                        {info.description && <div>{info.description}</div>}
+                        {info.address && (
+                          <div className="mt-4">
+                            <div className="text-uppercase">Certificate Store:</div>
+                            <a
+                              className="block text-gray-700 hover:text-gray-700 hover:underline break-words"
+                              href={`https://etherscan.io/address/${info.address}`}
+                              target="_blank"
+                              rel="noopener noreferrer"
+                            >
+                              {info.address}
+                            </a>
                           </div>
-                        )
-                    )}
-                  </div>
-                </div>
+                        )}
+                      </div>
+                    )
+                )}
               </div>
             </div>
           </div>
