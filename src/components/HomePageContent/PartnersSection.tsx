966e7413ca6da1c873d1cffc9616e01614c41c39
Sim Boon Long <simboonlong@gmail.com>
2020-11-11 11:17:09 +0800

    fix: mobile overflowing

--- src/components/HomePageContent/PartnersSection.tsx
+++ src/components/HomePageContent/PartnersSection.tsx
@@ -5,20 +5,14 @@ export const PartnerSection: React.FunctionComponent = () => (
   <section className="py-12 px-4">
     <div className="flex flex-wrap items-center justify-center">
       {Partners.map((item, i) => (
-        <div className="w-1/2 lg:w-auto text-center" key={i}>
+        <div className="w-auto text-center" key={i}>
           <a
-            className="inline-block p-8 hover:opacity-75 transition-opacity duration-200 ease-out"
+            className="inline-block p-4 md:p-8 hover:opacity-75 transition-opacity duration-200 ease-out"
             href={item.link}
             target="_blank"
             rel="noopener noreferrer"
           >
-            <img
-              className="mx-auto"
-              style={{ maxWidth: "150px", maxHeight: "100px" }}
-              src={item.value}
-              title={item.name}
-              alt={item.name}
-            />
+            <img className="mx-auto img-partner" src={item.value} title={item.name} alt={item.name} />
           </a>
         </div>
       ))}
