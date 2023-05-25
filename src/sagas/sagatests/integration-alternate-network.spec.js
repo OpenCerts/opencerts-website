80b35d3379e279465be149ac7454ba844d17e521
Lucas <31716292+yapyuyou@users.noreply.github.com>
2023-05-25 11:09:30 +0800

    test: add test for alternate network cert

---
+++ src/sagas/sagatests/integration-alternate-network.spec.js
@@ -0,0 +1,30 @@
+import { Selector } from "testcafe";
+import { waitForReact } from "testcafe-react-selectors";
+
+fixture("Alternate Network Cert").page`http://localhost:3000`.beforeEach(async () => {
+  await waitForReact();
+});
+
+const Certificate = "./sample-mumbai.opencert";
+
+const IframeBlock = Selector("#iframe");
+const SampleTemplate = Selector("#root");
+const StatusButton = Selector("#certificate-status");
+
+const validateTextContent = async (t, component, texts) =>
+  texts.reduce(async (prev, curr) => t.expect(component.textContent).contains(curr), Promise.resolve());
+
+test("Sample mumbai document is rendered correctly", async (t) => {
+  await t.setFilesToUpload("input[type=file]", [Certificate]);
+
+  await validateTextContent(t, StatusButton, ["DEMO-OPENCERTS.OPENATTESTATION.COM"]);
+
+  await t.switchToIframe(IframeBlock);
+
+  await validateTextContent(t, SampleTemplate, [
+    "OpenCerts Demo",
+    "Your Name",
+    "has successfully completed the",
+    "John Demo",
+  ]);
+});
