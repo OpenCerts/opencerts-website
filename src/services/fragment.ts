29dd8241afd6d742a78f9c9cc378d663a3e181e1
Kyle Huang Junyuan <kyle@hjunyuan.com>
2023-08-17 13:13:51 +0800

    chore: fix lint errors

--- src/services/fragment.ts
+++ src/services/fragment.ts
@@ -1,5 +1,4 @@
 import {
-  OpenAttestationDidSignedDocumentStatusVerificationFragment,
   OpenAttestationEthereumDocumentStoreStatusCode,
   OpenAttestationEthereumTokenRegistryStatusCode,
   VerificationFragment,
