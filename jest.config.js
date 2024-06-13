38ddef5885a5bbafb6e4206bd339b809668b15f2
Lucas <lucas_yap@tech.gov.sg>
2024-06-13 13:08:07 +0800

    fix: axios issue

--- jest.config.js
+++ jest.config.js
@@ -9,6 +9,7 @@ module.exports = {
     "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
       "<rootDir>/_mocks_/fileMock.js",
     "\\.(css|sass|scss)$": "<rootDir>/_mocks_/styleMock.js",
+    axios: "axios/dist/node/axios.cjs",
   },
   testResultsProcessor: "jest-sonar-reporter",
   watchPlugins: ["jest-watch-typeahead/filename", "jest-watch-typeahead/testname"],
