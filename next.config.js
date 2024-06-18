eca5f359e42acc957af67f9603a1c04222938521
tngzixiang <tng_zi_xiang@tech.gov.sg>
2024-06-18 10:29:46 +0800

    chore: add ga tag

--- next.config.js
+++ next.config.js
@@ -28,7 +28,7 @@ module.exports = withBundleAnalyzer({
     INFURA_API_KEY: process.env.INFURA_API_KEY || "84842078b09946638c03157f83405213", // ethers package default api key, feel free to change
     ALCHEMY_API_KEY: process.env.ALCHEMY_API_KEY || "FK1x9CdE8NStKjVt236D_LP7B6MMCFOs", // default key works on ropsten
     TRUSTED_TLDS: process.env.TRUSTED_TLDS || "gov.sg,edu.sg",
-    GA4_TAG_ID: process.env.GA4_TAG_ID,
+    GA4_TAG_ID: process.env.GA4_TAG_ID || "G-JP12T2F01V",
   },
   // Variables passed to both server and client
   publicRuntimeConfig: {
