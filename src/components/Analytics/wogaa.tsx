4f276ca4756759d8d5742dc0d8b04efd799b5f79
Kyle Huang Junyuan <kyle@hjunyuan.com>
2024-06-26 15:45:36 +0800

    refactor: add more comments

--- src/components/Analytics/wogaa.tsx
+++ src/components/Analytics/wogaa.tsx
@@ -1,9 +1,15 @@
 import React from "react";
 
-export const Wogaa = () =>
-  // NODE_ENV is automatically set by Next.js: https://nextjs.org/docs/pages/building-your-application/configuring/environment-variables#good-to-know
-  process.env.NODE_ENV === "production" ? (
-    <script src="https://assets.wogaa.sg/scripts/wogaa.js" />
-  ) : (
-    <script src="https://assets.dcube.cloud/scripts/wogaa.js" />
-  );
+export const Wogaa = () => (
+  <script
+    src={
+      // NODE_ENV is automatically set by Next.js: https://nextjs.org/docs/pages/building-your-application/configuring/environment-variables#good-to-know
+      // The allowed values for NODE_ENV: "production", "development" or "test"
+      // e.g. npm run dev -> NODE_ENV === "development"
+      // e.g. npm run build -> NODE_ENV === "production"
+      process.env.NODE_ENV === "production"
+        ? "https://assets.wogaa.sg/scripts/wogaa.js"
+        : "https://assets.dcube.cloud/scripts/wogaa.js"
+    }
+  />
+);
