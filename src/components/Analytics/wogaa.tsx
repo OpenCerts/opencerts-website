f8683c821668363eeaf1072696e39586a92d4d19
Kyle Huang Junyuan <kyle@hjunyuan.com>
2024-06-27 18:03:34 +0800

    fix: use dedicated WOGAA_ENV env var instead
    NODE_ENV cannot be overwritten

--- src/components/Analytics/wogaa.tsx
+++ src/components/Analytics/wogaa.tsx
@@ -35,11 +35,7 @@ export const completeTransactionalService: typeof window.wogaaCustom.completeTra
 export const Wogaa = () => (
   <script
     src={
-      // NODE_ENV is automatically set by Next.js: https://nextjs.org/docs/pages/building-your-application/configuring/environment-variables#good-to-know
-      // The allowed values for NODE_ENV: "production", "development" or "test"
-      // e.g. npm run dev -> NODE_ENV === "development"
-      // e.g. npm run build -> NODE_ENV === "production"
-      process.env.NODE_ENV === "production"
+      process.env.WOGAA_ENV === "production"
         ? "https://assets.wogaa.sg/scripts/wogaa.js"
         : "https://assets.dcube.cloud/scripts/wogaa.js"
     }
