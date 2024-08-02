21a7351cdd06379dfe46e78101bf1c6036dfeb07
Phan Shi Yu <phan_shi_yu@tech.gov.sg>
2024-08-02 18:51:52 +0800

    chore: always assert for component first

--- src/components/tests/utils.js
+++ src/components/tests/utils.js
@@ -1,2 +1,4 @@
-export const validateTextContent = async (t, component, texts) =>
-  Promise.all(texts.map(async (text) => await t.expect(component.textContent).contains(text)));
+export const validateTextContent = async (t, component, texts) => {
+  t.expect(component.exists).ok();
+  return Promise.all(texts.map(async (text) => await t.expect(component.textContent).contains(text)));
+};
