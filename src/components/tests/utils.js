5961484574e507358bf79ada1bf812534121f6fd
Phan Shi Yu <phan_shi_yu@tech.gov.sg>
2024-08-02 18:55:25 +0800

    fix: missing await

--- src/components/tests/utils.js
+++ src/components/tests/utils.js
@@ -1,4 +1,4 @@
 export const validateTextContent = async (t, component, texts) => {
-  t.expect(component.exists).ok();
-  return Promise.all(texts.map(async (text) => await t.expect(component.textContent).contains(text)));
+  await t.expect(component.exists).ok();
+  await Promise.all(texts.map(async (text) => await t.expect(component.textContent).contains(text)));
 };
