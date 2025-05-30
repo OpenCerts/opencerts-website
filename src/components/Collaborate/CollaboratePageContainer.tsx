04d89dc657986e799c3b953d48659a0893bdc46a
Kyle Huang Junyuan <kyle@hjunyuan.com>
2025-05-30 11:25:35 +0800

    chore: cleanup (#816)
    * chore: remove collaborate FormSG link
    
    * chore: remove technical query FormSG link

--- src/components/Collaborate/CollaboratePageContainer.tsx
+++ src/components/Collaborate/CollaboratePageContainer.tsx
@@ -14,18 +14,6 @@ export const CollaboratePage: React.FunctionComponent = () => (
         Do note that being on this list implies that these companies have demonstrated knowledge of implementing
         OpenCerts, but does not imply endorsement by GovTech.
       </p>
-      <p>
-        If you are from a company that can help to issue certificates and would like to be added to this list,
-        please&nbsp;
-        <a
-          className="underline"
-          href="https://form.gov.sg/5d01b2542ce4bb0011a86934"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          fill in this form.
-        </a>
-      </p>
     </Hero>
     <section className="py-8">
       <div className="container">
