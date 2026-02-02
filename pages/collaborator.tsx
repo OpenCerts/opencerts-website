91d089f14367c7b88f5aab517bd37a9ddacf0e72
rongquan1 <rongquan.low@gmail.com>
2026-02-02 11:29:56 +0800

    fix: lint

--- pages/collaborator.tsx
+++ pages/collaborator.tsx
@@ -10,11 +10,11 @@ import { URL } from "../src/config";
 const PAGE_SEO = {
   title: "Collaborator Terms and Conditions",
   description:
-    "Notice (administrative change): With effect from [01 Oct 2025], the OpenCerts Collaborator programme is administered by the Info-communications Media Development Authority (\"IMDA\"). These Collaborator Terms explain how to request listing and the obligations of the Collaboration.",
+    'Notice (administrative change): With effect from [01 Oct 2025], the OpenCerts Collaborator programme is administered by the Info-communications Media Development Authority ("IMDA"). These Collaborator Terms explain how to request listing and the obligations of the Collaboration.',
   openGraph: {
     title: "OpenCerts - Collaborator Terms and Conditions",
     description:
-      "Notice (administrative change): With effect from [01 Oct 2025], the OpenCerts Collaborator programme is administered by the Info-communications Media Development Authority (\"IMDA\"). These Collaborator Terms explain how to request listing and the obligations of the Collaboration.",
+      'Notice (administrative change): With effect from [01 Oct 2025], the OpenCerts Collaborator programme is administered by the Info-communications Media Development Authority ("IMDA"). These Collaborator Terms explain how to request listing and the obligations of the Collaboration.',
     url: `${URL}/collaborator`,
   },
 };
