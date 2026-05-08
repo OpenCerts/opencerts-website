405d6a3f7aefbf23b656c8c37a16e9cbb36d2001
manishdex25 <manish.singh@dextech.ai>
2026-05-08 14:41:24 +0530

    chore: add module name mapping for Sentry in Jest configuration
    - Included a module name mapper for "@sentry/nextjs" to use a mock implementation during testing.

---
+++ _mocks_/sentry-nextjs.js
@@ -0,0 +1,20 @@
+const noop = () => {};
+
+const Sentry = {
+  init: noop,
+  replayIntegration: () => ({}),
+  captureRouterTransitionStart: noop,
+  captureRequestError: noop,
+  captureUnderscoreErrorException: async () => undefined,
+  captureException: noop,
+  withScope: (callback) => {
+    callback({
+      setTag: noop,
+      setContext: noop,
+      setLevel: noop,
+    });
+  },
+  withSentryConfig: (config) => config,
+};
+
+module.exports = Sentry;
