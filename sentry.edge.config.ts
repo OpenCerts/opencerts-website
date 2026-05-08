6e65e3543b50f0b1863e5a44d721e58033124a29
manishdex25 <manish.singh@dextech.ai>
2026-05-08 13:10:16 +0530

    feat: integrate Sentry for error tracking and monitoring
    - Added Sentry configuration files for client, server, and edge environments.
    - Updated Babel configuration to include new plugins for class properties and private methods.
    - Created a custom error page to capture errors with Sentry.
    - Updated environment files and GitHub workflows to include Sentry authentication and configuration.
    - Added example environment file for Sentry integration.

---
+++ sentry.edge.config.ts
@@ -0,0 +1,20 @@
+// This file configures the initialization of Sentry for edge features (middleware, edge routes, and so on).
+// The config you add here will be used whenever one of the edge features is loaded.
+// Note that this config is unrelated to the Vercel Edge Runtime and is also required when running locally.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://c60c66ee81e19c382a615b8d396367af@o4511352989745152.ingest.us.sentry.io/4511353031491584",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Enable logs to be sent to Sentry
+  enableLogs: true,
+
+  // Enable sending user PII (Personally Identifiable Information)
+  // https://docs.sentry.io/platforms/javascript/guides/nextjs/configuration/options/#sendDefaultPii
+  sendDefaultPii: true,
+});
---
+++ sentry.server.config.ts
@@ -0,0 +1,19 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://c60c66ee81e19c382a615b8d396367af@o4511352989745152.ingest.us.sentry.io/4511353031491584",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Enable logs to be sent to Sentry
+  enableLogs: true,
+
+  // Enable sending user PII (Personally Identifiable Information)
+  // https://docs.sentry.io/platforms/javascript/guides/nextjs/configuration/options/#sendDefaultPii
+  sendDefaultPii: true,
+});
