7d5339372523ac92068b34dea5274636bcd73c7e
manishdex25 <manish.singh@dextech.ai>
2026-05-08 13:48:16 +0530

    chore: sync new commit

--- sentry.edge.config.ts
+++ sentry.edge.config.ts
@@ -12,7 +12,7 @@ Sentry.init({
   tracesSampleRate: process.env.NODE_ENV === "development" ? 1 : 0.1,
 
   // Enable logs to be sent to Sentry
-  enableLogs: process.env.NODE_ENV === "development",
+  enableLogs: process.env.NODE_ENV === 'development',
 
   // https://docs.sentry.io/platforms/javascript/guides/nextjs/configuration/options/#sendDefaultPii
   sendDefaultPii: true,
